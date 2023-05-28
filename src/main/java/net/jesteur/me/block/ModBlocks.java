feef1d8a214f0bb36cef482810373f0e4ed1aab0
nullBlade <stepan.gaidukevich@gmail.com>
2023-05-28 08:36:01 +0300

    added comments and @SuppressWarnings("unused")

--- src/main/java/net/jesteur/me/block/ModBlocks.java
+++ src/main/java/net/jesteur/me/block/ModBlocks.java
@@ -13,12 +13,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+@SuppressWarnings("unused")
 public class ModBlocks {
     public static final float STONE_STRENGTH = 1.5f;
     public static final float WOOD_STRENGTH = 2f;
     public static final float DIRT_STRENGTH = 0.6f;
     public static final float SLAB_RESISTANCE = 6.0f; // From explosions
 
+    // Gondor Bricks
     public static final Block GONDOR_BRICKS = registerBlock("gondor_bricks",
             new Block(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH).requiresTool()));
     public static final Block GONDOR_BRICKS_SLAB = registerBlock("gondor_bricks_slab",
@@ -28,15 +30,19 @@ public class ModBlocks {
     public static final Block GONDOR_BRICKS_WALL = registerBlock("gondor_bricks_wall",
             new WallBlock(AbstractBlock.Settings.copy(GONDOR_BRICKS).strength(STONE_STRENGTH).requiresTool()));
 
+    // Blue Rock
     public static final Block BLUE_ROCK = registerBlock("blue_rock",
             new Block(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH, SLAB_RESISTANCE).requiresTool()));
     public static final Block BLUE_ROCK_SLAB = registerBlock("blue_rock_slab",
             new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH).requiresTool()));
 
+    // Ashen Rock
     public static final Block ASHEN_ROCK = registerBlock("ashen_rock",
             new Block(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH).requiresTool()));
     public static final Block ASHEN_ROCK_SLAB = registerBlock("ashen_rock_slab",
             new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH, SLAB_RESISTANCE).requiresTool()));
+
+    // Ashen Bricks
     public static final Block ASHEN_BRICKS = registerBlock("ashen_bricks",
             new Block(FabricBlockSettings.of(Material.STONE).strength(STONE_STRENGTH).requiresTool()));
     public static final Block ASHEN_BRICKS_SLAB = registerBlock("ashen_bricks_slab",
@@ -46,15 +52,16 @@ public class ModBlocks {
     public static final Block ASHEN_BRICKS_WALL = registerBlock("ashen_bricks_wall",
             new WallBlock(AbstractBlock.Settings.copy(ASHEN_BRICKS).strength(STONE_STRENGTH).requiresTool()));
 
+
     public static final Block MORDOR_DIRT = registerBlock("mordor_dirt",
             new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC).strength(DIRT_STRENGTH).sounds(BlockSoundGroup.GRAVEL)));
 
+    // Mallorn
     public static final Block MALLORN_LEAVES = registerBlock("mallorn_leaves",
             new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES)));
     public static final Block MALLORN_LOG = registerBlock("mallorn_log",
             new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG).strength(WOOD_STRENGTH).requiresTool()
                     .sounds(BlockSoundGroup.WOOD)));
-
     public static final Block MALLORN_PLANKS = registerBlock("mallorn_planks",
             new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).strength(WOOD_STRENGTH).requiresTool()
                     .sounds(BlockSoundGroup.WOOD)));
@@ -62,11 +69,15 @@ public class ModBlocks {
             new SlabBlock(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).strength(WOOD_STRENGTH, SLAB_RESISTANCE).requiresTool().sounds(BlockSoundGroup.WOOD)));
     public static final Block MALLORN_PLANKS_STAIRS = registerBlock("mallorn_planks_stairs",
             new StairsBlock(MALLORN_PLANKS.getDefaultState(), AbstractBlock.Settings.copy(MALLORN_PLANKS).strength(WOOD_STRENGTH).requiresTool()));
+
+    // Mithril
     public static final Block MITHRIL_BLOCK = registerBlock("mithril_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()));
     public static final Block MITHRIL_ORE = registerBlock("mithril_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()));
 
+
+    // Helper functions
     public static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(MiddleEarth.MOD_ID, name), block);
