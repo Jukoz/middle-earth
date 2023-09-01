4b2789606c1414f14b3897f54651b091fed43c60
krahab <remygarenc@gmail.com>
2023-09-01 15:55:42 -0400

    backup

--- src/main/java/net/jesteur/me/entity/equipments/CloakCapeEntityModel.java
+++ src/main/java/net/jesteur/me/entity/equipments/CloakCapeEntityModel.java
@@ -18,8 +18,8 @@ public class CloakCapeEntityModel<T extends LivingEntity> extends AnimalModel<T>
         ModelPartData modelPartData = modelData.getRoot();
         Dilation dilation = new Dilation(0.0F);
         modelPartData.addChild("cape", ModelPartBuilder.create().uv(32, 32)
-                .cuboid(-6.0F, -1F, 3f, 12.0F, 22.0F, 1.0F, dilation)
-                , ModelTransform.of(0, 0, 0, 0.0872665f, 0.0F, 0F));
+                .cuboid(-6.0F, -1F, -2f, 12.0F, 22.0F, 1.0F, dilation)
+                , ModelTransform.of(0, 1f, 5f, 0.0F, 0.0F, 0F));
         return TexturedModelData.of(modelData, 32, 32);
     }
 
@@ -33,5 +33,12 @@ public class CloakCapeEntityModel<T extends LivingEntity> extends AnimalModel<T>
 
     @Override
     public void setAngles(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+        if(entity.getVelocity().x != 0 || entity.getVelocity().y != 0){
+            this.cape.pitch = (float)Math.sin(45);
+        } else if (entity.getVelocity().y != 0){
+            this.cape.pitch = (float)Math.sin(90);
+        } else {
+            this.cape.pitch = Math.abs((float)Math.sin(animationProgress * 0.2f));
+        }
     }
 }
