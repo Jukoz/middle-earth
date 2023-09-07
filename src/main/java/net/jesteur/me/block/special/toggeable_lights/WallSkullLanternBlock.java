02aeaa875bd73bec134e4609464c9f19901e9f83
krahab <remygarenc@gmail.com>
2023-09-07 13:23:03 -0400

    Added placement logic

--- src/main/java/net/jesteur/me/block/special/toggeable_lights/WallSkullLanternBlock.java
+++ src/main/java/net/jesteur/me/block/special/toggeable_lights/WallSkullLanternBlock.java
@@ -18,10 +18,8 @@ import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
+import net.minecraft.state.property.*;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -37,8 +35,61 @@ import org.jetbrains.annotations.Nullable;
 import java.util.function.ToIntFunction;
 
 public class WallSkullLanternBlock extends SkullLanternBlock {
+    private static final DirectionProperty FACING;
 
     public WallSkullLanternBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.EAST));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+        super.appendProperties(builder);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        Direction[] var3 = ctx.getPlacementDirections();
+        int var4 = var3.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Direction direction = var3[var5];
+            System.out.print(direction);
+            BlockState blockState = this.getDefaultState()
+                    .with(HANGING, false)
+                    .with(FACING, direction.getOpposite());
+            if (blockState.canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) {
+                if(!ctx.getWorld().isClient())
+                    ctx.getWorld().getPlayers().get(0).sendMessage(Text.literal("Placing block : " + direction));
+                return blockState.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        Direction direction = attachedDirection(state).getOpposite();
+        return Block.sideCoversSmallSquare(world, pos.offset(direction), direction.getOpposite());
+    }
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            extinguish(null, state, world, pos);
+        }
+
+        return attachedDirection(state).getOpposite() == direction && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+    protected static Direction attachedDirection(BlockState state) {
+        return state.get(FACING);
+    }
+
+    static {
+        FACING = Properties.FACING;
     }
 }
--- src/main/java/net/jesteur/me/item/ModDecorativeItems.java
+++ src/main/java/net/jesteur/me/item/ModDecorativeItems.java
@@ -1,14 +1,10 @@
 package net.jesteur.me.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.jesteur.me.MiddleEarth;
 import net.jesteur.me.block.ModDecorativeBlocks;
-import net.jesteur.me.item.utils.ModArmorMaterials;
 import net.jesteur.me.item.utils.ModItemGroups;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.ArmorItem;
+import net.jesteur.me.item.utils.ModVerticallyAttachableBlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.VerticallyAttachableBlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,7 +12,7 @@ import net.minecraft.util.math.Direction;
 
 public class ModDecorativeItems {
     public static final Item SKULL_LANTERN = registerItem("skull_lantern",
-            new VerticallyAttachableBlockItem(ModDecorativeBlocks.SKULL_LANTERN, ModDecorativeBlocks.WALL_SKULL_LANTERN, new Item.Settings(), Direction.DOWN));
+            new ModVerticallyAttachableBlockItem(ModDecorativeBlocks.SKULL_LANTERN, ModDecorativeBlocks.WALL_SKULL_LANTERN, new Item.Settings(), new Direction[]{Direction.DOWN, Direction.UP}));
 
 
     private static Item registerItem(String name, Item item) {
