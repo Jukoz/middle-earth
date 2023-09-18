60837c7477420b50b38fedd8d0824f9a4ebf7582
Edvin Frosterud <edvin.frosterud@gmail.com>
2023-09-18 22:59:23 +0200

    Update AbstractClientPlayerEntityMixin.java

--- src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -2,14 +2,21 @@ package net.jesteur.me.mixin.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.jesteur.me.entity.barrow_wights.BarrowWightEntity;
 import net.jesteur.me.statusEffects.ModStatusEffects;
 import net.jesteur.me.utils.HallucinationData;
 import net.jesteur.me.utils.IEntityDataSaver;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,6 +30,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class AbstractClientPlayerEntityMixin {
 
 
+    @Shadow @Final public ClientWorld clientWorld;
+
     @Inject(method = "getFovMultiplier()F", at =@At("TAIL"), cancellable = true)
     private void injected(CallbackInfoReturnable<Float> cir) {
     PlayerEntity player = MinecraftClient.getInstance().player;
@@ -30,6 +39,11 @@ public abstract class AbstractClientPlayerEntityMixin {
         //player.changeLookDirection(2, 2);
 
         if(player.hasStatusEffect(ModStatusEffects.HALLUCINATION)){
+            LivingEntity lookAt = clientWorld.getClosestEntity(BarrowWightEntity.class, TargetPredicate.DEFAULT, player, player.getX(), player.getY(), player.getZ(), new Box(5, 5, 5, 5, 5,5));
+            if( lookAt != null ){
+                MinecraftClient.getInstance().player.sendMessage(Text.literal("Closest Entity UUID: " + lookAt.getUuidAsString()));
+                player.lookAt(player.getCommandSource().getEntityAnchor(), new Vec3d(lookAt.getX(), lookAt.getEyeY(), lookAt.getZ()));
+            }
             float intensity = (float) HallucinationData.readHallucination((IEntityDataSaver) player) / 100f;
             cir.setReturnValue(cir.getReturnValue() * (1 - intensity/4));
 
