07be1a755f733403ff9da81560440955b0044c19
Jukoz <olegptk12@gmail.com>
2023-09-19 13:02:04 -0400

    Testing focus look

--- src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.command.argument.EntityAnchorArgumentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.player.PlayerEntity;
@@ -18,6 +19,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,10 +28,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Environment(EnvType.CLIENT)
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin {
 
+    @Shadow @Final public ClientWorld clientWorld;
 
     @Shadow @Final public ClientWorld clientWorld;
 
@@ -49,6 +54,42 @@ public abstract class AbstractClientPlayerEntityMixin {
             cir.setReturnValue(cir.getReturnValue() * (1 - intensity/4));
 
         }
+
+        LivingEntity lookAt = clientWorld.getClosestEntity(BarrowWightEntity.class, TargetPredicate.createAttackable(), player, player.getX(), player.getY(), player.getZ(),
+                player.getBoundingBox().expand(12.0, 12.0, 12.0));
+        //List<Entity> list = this.clientWorld.getOtherEntities(player, new Box(-15, -15, -15, 15, 15,15));
+        //System.out.println(lookAt);
+        if(lookAt != null) {
+            double dX = lookAt.getX() - player.getX(); // d
+            double dY = lookAt.getY() - player.getY(); // e
+            double dZ = lookAt.getZ() - player.getZ(); // f
+            double g = Math.sqrt(dX * dX + dZ * dZ);
+
+            float destPitch = MathHelper.wrapDegrees((float)(-(MathHelper.atan2(dY, g) * 57.2957763671875)));
+            float destYaw = MathHelper.wrapDegrees((float)(MathHelper.atan2(dZ, dX) * 57.2957763671875) - 90.0f);
+
+            //destPitch = MathHelper.lerp(player.getPitch(), destPitch, 0.1f);
+            //destYaw = MathHelper.lerp(player.getYaw(), destYaw, 0.1f);
+
+            //destPitch = player.getPitch();
+            //destYaw = player.getYaw();
+
+            player.setPitch(destPitch);
+            player.setYaw(destYaw);
+            player.setHeadYaw(player.getYaw());
+            player.prevPitch = player.getPitch();
+            player.prevYaw = player.getYaw();
+
+            Vec3d currentLookAt = player.getCameraPosVec(0);
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("Dest Yaw: " + destYaw + " while player: " + player.getYaw()));
+
+            //float lookAtX = MathCurves.lerp(player.getYaw(), (float) lookAt.getX(), 0.01f);
+            //float lookAtY = MathCurves.lerp((float) currentLookAt.getY(), (float) lookAt.getEyeY(), 0.01f);
+            //float lookAtZ = MathCurves.lerp((float) currentLookAt.getZ(), (float) lookAt.getZ(), 0.01f);
+            //player.lookAt(player.getCommandSource().getEntityAnchor(), new Vec3d(lookAtX, lookAtY, lookAtZ)); // lookAt.getEyeY()
+            //player.setPitch((float) MathHelper.lerp(player.getPitch(), destPitch, 0.1f));
+        }
+
     }
 
 }
