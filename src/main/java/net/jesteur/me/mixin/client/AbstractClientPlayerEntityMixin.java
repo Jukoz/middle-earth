a7248757dd0fa673fbd05f3979f1d746dff558e6
Edvin Frosterud <edvin.frosterud@gmail.com>
2023-09-19 21:14:01 +0200

    Doesn't jitter between two mobs.
    + Only works when potion effect is applied.
    + Saves entity and locks player on closest entity. When potion effect is applied by projectile should probably do that instead, but doesn't get overwritten.

--- src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/jesteur/me/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -35,59 +35,50 @@ import java.util.List;
 public abstract class AbstractClientPlayerEntityMixin {
 
     @Shadow @Final public ClientWorld clientWorld;
-
+    LivingEntity lookAt;
 
     @Inject(method = "getFovMultiplier()F", at =@At("TAIL"), cancellable = true)
     private void injected(CallbackInfoReturnable<Float> cir) {
     PlayerEntity player = MinecraftClient.getInstance().player;
         assert player != null;
         //player.changeLookDirection(2, 2);
-
         if(player.hasStatusEffect(ModStatusEffects.HALLUCINATION)){
-            /*LivingEntity lookAt = clientWorld.getClosestEntity(BarrowWightEntity.class, TargetPredicate.createNonAttackable(), null, player.getX(), player.getY(), player.getZ(), player.getBoundingBox().expand(5));
             if( lookAt != null ){
-                MinecraftClient.getInstance().player.sendMessage(Text.literal("Closest Entity UUID: " + lookAt.getUuidAsString()));
-                player.lookAt(player.getCommandSource().getEntityAnchor(), new Vec3d(lookAt.getX(), lookAt.getEyeY()-1.4f, lookAt.getZ()));
-            }*/
+                double dX = lookAt.getX() - player.getX(); // d
+                double dY = lookAt.getY() - player.getY(); // e
+                double dZ = lookAt.getZ() - player.getZ(); // f
+                double g = Math.sqrt(dX * dX + dZ * dZ);
+
+                float destPitch = MathHelper.wrapDegrees((float)(-(MathHelper.atan2(dY, g) * 57.2957763671875)));
+                float destYaw = MathHelper.wrapDegrees((float)(MathHelper.atan2(dZ, dX) * 57.2957763671875) - 90.0f);
+
+                destPitch = MathHelper.lerp(1/20f, destPitch, player.getPitch());
+                destYaw = MathHelper.lerp(1/20f, destYaw, player.getYaw());
+                //player.changeLookDirection(1920/Math.sin(destYaw), 1080/Math.sin(destPitch)); cant figure this out.
+
+                //destPitch = player.getPitch();
+                //destYaw = player.getYaw();
+                player.setPitch(destPitch);
+                player.setYaw(destYaw);
+                player.setHeadYaw(player.getYaw());
+                player.prevPitch = player.getPitch();
+                player.prevYaw = player.getYaw();
+
+                //Vec3d currentLookAt = player.getCameraPosVec(0);
+                //MinecraftClient.getInstance().player.sendMessage(Text.literal("Dest Yaw: " + destYaw + " while player: " + player.getYaw()));
+
+            }
+            else
+                lookAt = clientWorld.getClosestEntity(BarrowWightEntity.class, TargetPredicate.createNonAttackable(), null, player.getX(), player.getY(), player.getZ(), player.getBoundingBox().expand(12));
+
             float intensity = (float) HallucinationData.readHallucination((IEntityDataSaver) player) / 100f;
             cir.setReturnValue(cir.getReturnValue() * (1 - intensity/4));
 
         }
+        else
+            lookAt = null;
 
-        LivingEntity lookAt = clientWorld.getClosestEntity(BarrowWightEntity.class, TargetPredicate.createAttackable(), player, player.getX(), player.getY(), player.getZ(),
-                player.getBoundingBox().expand(12.0, 12.0, 12.0));
-        //List<Entity> list = this.clientWorld.getOtherEntities(player, new Box(-15, -15, -15, 15, 15,15));
-        //System.out.println(lookAt);
-        if(lookAt != null) {
-            double dX = lookAt.getX() - player.getX(); // d
-            double dY = lookAt.getY() - player.getY(); // e
-            double dZ = lookAt.getZ() - player.getZ(); // f
-            double g = Math.sqrt(dX * dX + dZ * dZ);
 
-            float destPitch = MathHelper.wrapDegrees((float)(-(MathHelper.atan2(dY, g) * 57.2957763671875)));
-            float destYaw = MathHelper.wrapDegrees((float)(MathHelper.atan2(dZ, dX) * 57.2957763671875) - 90.0f);
-
-            destPitch = MathHelper.lerp(0.1f, destPitch, player.getPitch());
-            destYaw = MathHelper.lerp(0.1f, destYaw, player.getYaw());
-
-            //destPitch = player.getPitch();
-            //destYaw = player.getYaw();
-
-            player.setPitch(destPitch);
-            player.setYaw(destYaw);
-            player.setHeadYaw(player.getYaw());
-            player.prevPitch = player.getPitch();
-            player.prevYaw = player.getYaw();
-
-            Vec3d currentLookAt = player.getCameraPosVec(0);
-            MinecraftClient.getInstance().player.sendMessage(Text.literal("Dest Yaw: " + destYaw + " while player: " + player.getYaw()));
-
-            //float lookAtX = MathCurves.lerp(player.getYaw(), (float) lookAt.getX(), 0.01f);
-            //float lookAtY = MathCurves.lerp((float) currentLookAt.getY(), (float) lookAt.getEyeY(), 0.01f);
-            //float lookAtZ = MathCurves.lerp((float) currentLookAt.getZ(), (float) lookAt.getZ(), 0.01f);
-            //player.lookAt(player.getCommandSource().getEntityAnchor(), new Vec3d(lookAtX, lookAtY, lookAtZ)); // lookAt.getEyeY()
-            //player.setPitch((float) MathHelper.lerp(player.getPitch(), destPitch, 0.1f));
-        }
 
     }
 
