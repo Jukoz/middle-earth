910dd211c9c272141dd23ed8b3565b739d4c8704
Jukoz <olegptk12@gmail.com>
2023-10-10 18:11:02 -0400

    Added achievements

--- src/main/java/net/jesteur/me/mixin/BlockEntityLandedMixin.java
+++ src/main/java/net/jesteur/me/mixin/BlockEntityLandedMixin.java
@@ -29,9 +29,6 @@ import java.util.List;
 
 @Mixin(Block.class)
 public abstract class BlockEntityLandedMixin {
-	private static final float RANDOM_PARTICLE_RANGE = 0.25f;
-	private static final float PARTICLE_VELOCITY = 0.25f;
-	private static final int PARTICLE_QUANTITY = 15;
 	private static final float ITEM_SPAWN_Y_OFFSET = 0.75f;
 	private static final float ITEM_CRAFT_RANGE = 2f;
 
@@ -39,7 +36,7 @@ public abstract class BlockEntityLandedMixin {
 	 *
 	 * **/
 	@Inject(method = "onLandedUpon", at = @At(value = "HEAD"))
-	private void damage(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+	private void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
 
 		if(entity.getType() == EntityType.ITEM) {
 			ItemEntity entityItem = (ItemEntity) entity;
@@ -51,10 +48,6 @@ public abstract class BlockEntityLandedMixin {
 
 					for (ItemEntity itm : entities) {
 						if (itm.getStack().getItem() == Items.GLOWSTONE_DUST || itm.getStack().getItem() == Items.GLOW_INK_SAC) {
-
-							world.playSound(position.getX(), position.getY(), position.getZ(), SoundEvents.BLOCK_BREWING_STAND_BREW, SoundCategory.BLOCKS, 1, 1.2f, true);
-
-							ParticleManager.createParticles(world, entityItem.getPos(), ParticleTypes.END_ROD);
 							removeOneItemFromStackEntity(itm);
 							removeOneItemFromStackEntity(entityItem);
 
@@ -67,6 +60,9 @@ public abstract class BlockEntityLandedMixin {
 									serverWorld.spawnEntityAndPassengers(newItem);
 									newItem.refreshPositionAndAngles(entity.getX(), entity.getY() + ITEM_SPAWN_Y_OFFSET, entity.getZ(), 0, 0);
 								}
+							} else {
+								ParticleManager.createParticles(world, entityItem.getPos(), ParticleTypes.END_ROD);
+								world.playSound(position.getX(), position.getY(), position.getZ(), SoundEvents.BLOCK_BREWING_STAND_BREW, SoundCategory.BLOCKS, 1, 1.2f, true);
 							}
 							break;
 						}
