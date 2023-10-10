245103012d2386545b6e52772e5462ee17d1cf43
Jukoz <olegptk12@gmail.com>
2023-10-10 11:02:56 -0400

    Better trunk gen

--- src/main/java/net/jesteur/me/world/features/BigTrunkPlacer.java
+++ src/main/java/net/jesteur/me/world/features/BigTrunkPlacer.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BigTrunkPlacer extends TrunkPlacer {
+    public static final float TRUNK_ANGLE = 4.2f;
     public static final Codec<BigTrunkPlacer> CODEC = RecordCodecBuilder.create(instance ->
             fillTrunkPlacerFields(instance).apply(instance, BigTrunkPlacer::new));
 
@@ -37,65 +38,49 @@ public class BigTrunkPlacer extends TrunkPlacer {
         setToDirt(world, replacer, random, blockPos.south(), config);
         setToDirt(world, replacer, random, blockPos.south().east(), config);
         BlockPos.Mutable mutable = new BlockPos.Mutable();
-        //double radius = 2.5f;
-        //final int square = (int) Math.ceil(radius);
-        //for(int i = 0; i < height; ++i) {
-        //    for (int dx = -1; dx <= square; dx++) {
-        //        for (int dz = -1; dz <= square; dz++) {
-        //            double radiusSquared = radius * radius;
-        //            double x = dx - radius;
-        //            double z = dz - radius;
-        //            double distanceSquared = x * x + z * z;
-        //            if(distanceSquared <= radiusSquared) this.setLog(world, replacer, random, mutable, config, startPos, dx, i, dz);
-        //        }
-        //    }
-        //    float halfHeight = (float)height / 2;
-        //    if(i < height / 2) {
-        //        radius = lerp(1.6f, 2.5f, i / halfHeight);
-        //    } else {
-        //        radius = lerp(2.5f, 1.6f, (i - halfHeight) / halfHeight);
-        //    }
-        //}
-        double radius = 2.7;
-        final int ceilRadius = (int) Math.ceil(radius);
-        for (int i = 0; i < height; ++i) {
-            for (int dx = -ceilRadius; dx <= ceilRadius; dx++) {
-                for (int dz = -ceilRadius; dz <= ceilRadius; dz++) {
-                    //double x = dx + 0.5;
-                    //double z = dz + 0.5;
-                    double distanceSquared = dx * dx + dz * dz;
-                    distanceSquared += (-0.45f + Math.random() * 0.5f);
 
-                    if (distanceSquared <= radius * radius) {
-                        this.setLog(world, replacer, random, mutable, config, startPos, dx, i, dz);
-                    }
-                }
-            }
+        double angle = Math.random() * (360/Math.PI);
+        float radius = 2.7f;
 
-            float halfHeight = (float) height / 2;
-            if (i < height * 0.4f) {
-                radius = lerp(2.5f, 1.2f, (i / halfHeight));
-            } else {
-                radius = lerp(1.2f, 2.3f, ((i - halfHeight) / halfHeight));
-            }
-        }
-        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(height), 0, true));
+        createBranch(world, replacer, random, mutable, config, startPos, height, angle, radius, 0.6f);
+
+        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(height)
+                .add((int)(Math.cos(angle) * TRUNK_ANGLE), 0, (int)(Math.sin(angle) * TRUNK_ANGLE)), 0, true));
     }
 
 
-    public static double interpolate(double a, double b, float percentage) {
-        double normalizedHeight = (double) percentage / a;
-        double lerpFactor = Math.abs(normalizedHeight - 1);
+    private void createBranch(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos.Mutable mutable,
+                        TreeFeatureConfig config, BlockPos startPos, int height, double angle, float radiusA, float radiusB) {
+        float radius = radiusA;
+        final int ceilRadius = (int) Math.ceil(radiusA);
+        for (int i = 0; i < height; ++i) {
+            float percentage = (float) (Math.pow((float) i / height, 1.2));
+            float offsetX = lerp((float) 0, (float) Math.cos(angle), percentage) * TRUNK_ANGLE;
+            float offsetZ = lerp((float) 0, (float) Math.sin(angle), percentage) * TRUNK_ANGLE;
+            for (int x = -ceilRadius; x <= ceilRadius; x++) {
+                for (int z = -ceilRadius; z <= ceilRadius; z++) {
+                    double dx = x;
+                    double dz = z;
+                    double distanceSquared = x * x + z * z;
+                    distanceSquared += (Math.random() * -0.51f);
 
-        // Use linear interpolation to smoothly transition from A to B and back to A
-        return a + lerpFactor * (b - a);
+                    if (distanceSquared <= radius * radius) {
+                        dx += offsetX;
+                        dz += offsetZ;
+                        this.setLog(world, replacer, random, mutable, config, startPos, (int) dx, i, (int) dz);
+                    }
+                }
+            }
+            radius = lerp(2.7f, radiusB, (float) i / height);
+        }
     }
 
     public static float lerp(float a, float b, float interpolation) {
         return a + interpolation * (b - a);
     }
 
-    private void setLog(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos.Mutable tmpPos, TreeFeatureConfig config, BlockPos startPos, int dx, int dy, int dz) {
+    private void setLog(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos.Mutable tmpPos,
+                        TreeFeatureConfig config, BlockPos startPos, int dx, int dy, int dz) {
         tmpPos.set(startPos, dx, dy, dz);
         this.trySetState(world, replacer, random, tmpPos, config);
     }
