10a2945095d9a1cb0aad067919e3b37f21fda277
Jukoz <olegptk12@gmail.com>
2023-10-12 16:14:33 -0400

    Fog transition fix

--- src/main/java/net/jesteur/me/mixin/client/BackgroundRendererMixin.java
+++ src/main/java/net/jesteur/me/mixin/client/BackgroundRendererMixin.java
@@ -8,10 +8,11 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.FogShape;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,8 +24,9 @@ import java.util.Optional;
 @Environment(EnvType.CLIENT)
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
-    private float currentFogStart = 0;
-    private float currentFogEnd = 0;
+    private static final float TICK_SPEED = 0.001f;
+    private static float fogStartMultiplier = 1;
+    private static float fogEndMultiplier = 1;
 
     @Inject(method = "applyFog", at = @At("TAIL"))
     private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
@@ -35,10 +37,27 @@ public class BackgroundRendererMixin {
                 Optional<RegistryKey<Biome>> biomeRegistry = clientPlayerEntity.getWorld().getBiome(clientPlayerEntity.getBlockPos()).getKey();
                 if(biomeRegistry.isPresent() && MEBiomeFogData.DATA.containsKey(biomeRegistry.get())){
                     MEBiomeFogData fogData = MEBiomeFogData.DATA.get(biomeRegistry.get());
-                    RenderSystem.setShaderFogStart(fogData.fogStart);
-                    RenderSystem.setShaderFogEnd(fogData.fogEnd);
-                    RenderSystem.setShaderFogShape(FogShape.SPHERE);
+                    
+                    if (fogStartMultiplier < fogData.fogStart) {
+                        fogStartMultiplier = Math.min(fogStartMultiplier + (tickDelta * TICK_SPEED), fogData.fogStart);
+                    } else if (fogStartMultiplier > fogData.fogStart) {
+                        fogStartMultiplier = Math.max(fogStartMultiplier - (tickDelta * TICK_SPEED), fogData.fogStart);
+                    }
+
+                    if (fogEndMultiplier < fogData.fogEnd) {
+                        fogEndMultiplier = Math.min(fogEndMultiplier + (tickDelta * TICK_SPEED), fogData.fogEnd);
+                    } else if (fogEndMultiplier > fogData.fogEnd) {
+                        fogEndMultiplier = Math.max(fogEndMultiplier - (tickDelta * TICK_SPEED), fogData.fogEnd);
+                    }
                 }
+
+                float f = MathHelper.clamp(viewDistance / 10.0F, 4.0F, 64.0F);
+                float fogStart = (viewDistance - f) * fogStartMultiplier;
+                float fogEnd = viewDistance * fogEndMultiplier;
+
+                RenderSystem.setShaderFogStart(fogStart);
+                RenderSystem.setShaderFogEnd(fogEnd);
+                RenderSystem.setShaderFogShape(FogShape.SPHERE);
             }
 
         }
--- src/main/java/net/jesteur/me/world/biomes/MEBiomeFogData.java
+++ src/main/java/net/jesteur/me/world/biomes/MEBiomeFogData.java
@@ -12,20 +12,20 @@ public class MEBiomeFogData {
     public static Map<RegistryKey<Biome>, MEBiomeFogData> DATA;
     static {
         DATA = new HashMap<>();
-        DATA.put(MEBiomeKeys.ERIADOR, new MEBiomeFogData(40f, 200f));
-        DATA.put(MEBiomeKeys.BARROW_DOWNS, new MEBiomeFogData(15f, 80f));
-        DATA.put(MEBiomeKeys.DARK_MIRKWOOD, new MEBiomeFogData(15f, 200f));
-        DATA.put(MEBiomeKeys.DARK_MIRKWOOD_EDGE, new MEBiomeFogData(40f, 200f));
-        DATA.put(MEBiomeKeys.DOL_GULDUR, new MEBiomeFogData(15f, 200f));
-        DATA.put(MEBiomeKeys.DUNLAND_FOOTHILLS, new MEBiomeFogData(-25f, 96f));
-        DATA.put(MEBiomeKeys.FANGORN, new MEBiomeFogData(15f, 170F));
-        DATA.put(MEBiomeKeys.FORODWAITH, new MEBiomeFogData(10f, 148F));
-        DATA.put(MEBiomeKeys.MIRKWOOD, new MEBiomeFogData(15f, 200f));
-        DATA.put(MEBiomeKeys.MIRKWOOD_EDGE, new MEBiomeFogData(40f, 200f));
-        DATA.put(MEBiomeKeys.MIRKWOOD_FOOTHILLS, new MEBiomeFogData(-5f, 125f));
-        DATA.put(MEBiomeKeys.MISTY_MOUNTAINS, new MEBiomeFogData(-50f, 52f));
-        DATA.put(MEBiomeKeys.NORTHERN_DUNLAND, new MEBiomeFogData(20f, 200f));
-        DATA.put(MEBiomeKeys.THE_OLD_FOREST, new MEBiomeFogData(25f, 200F));
+        DATA.put(MEBiomeKeys.ERIADOR, new MEBiomeFogData(0.9f, 0.95f));
+        DATA.put(MEBiomeKeys.BARROW_DOWNS, new MEBiomeFogData(0.4f, 0.6f));
+        DATA.put(MEBiomeKeys.DARK_MIRKWOOD, new MEBiomeFogData(0.65f, 0.8f));
+        DATA.put(MEBiomeKeys.DARK_MIRKWOOD_EDGE, new MEBiomeFogData(0.75f, 0.85f));
+        DATA.put(MEBiomeKeys.DOL_GULDUR, new MEBiomeFogData(0.8f, 0.9f));
+        DATA.put(MEBiomeKeys.DUNLAND_FOOTHILLS, new MEBiomeFogData(0.9f, 0.95f));
+        DATA.put(MEBiomeKeys.FANGORN, new MEBiomeFogData(0.7f, 0.9f));
+        DATA.put(MEBiomeKeys.FORODWAITH, new MEBiomeFogData(0.9f, 0.95f));
+        DATA.put(MEBiomeKeys.MIRKWOOD, new MEBiomeFogData(0.7f, 0.85f));
+        DATA.put(MEBiomeKeys.MIRKWOOD_EDGE, new MEBiomeFogData(0.85f, 0.9f));
+        DATA.put(MEBiomeKeys.MIRKWOOD_FOOTHILLS, new MEBiomeFogData(0.85f, 0.95f));
+        DATA.put(MEBiomeKeys.MISTY_MOUNTAINS, new MEBiomeFogData(0.3f, 0.6f));
+        DATA.put(MEBiomeKeys.NORTHERN_DUNLAND, new MEBiomeFogData(0.8f, 0.9f));
+        DATA.put(MEBiomeKeys.THE_OLD_FOREST, new MEBiomeFogData(0.7f, 0.8f));
     };
 
     public float fogStart;
