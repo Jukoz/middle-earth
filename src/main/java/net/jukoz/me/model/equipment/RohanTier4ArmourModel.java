b09831d59da70604776920c5544a57c25dcac5de
Jukoz <olegptk12@gmail.com>
2024-01-29 15:10:35 -0500

    Fixes armor stand armor rotations

--- src/main/java/net/jukoz/me/model/equipment/RohanTier4ArmourModel.java
+++ src/main/java/net/jukoz/me/model/equipment/RohanTier4ArmourModel.java
@@ -3,6 +3,7 @@ package net.jukoz.me.model.equipment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 
 public class RohanTier4ArmourModel<T extends LivingEntity> extends BipedEntityModel<T> {
 
@@ -18,19 +19,56 @@ public class RohanTier4ArmourModel<T extends LivingEntity> extends BipedEntityMo
         modelPartData.addChild("head", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
         modelPartData.addChild("body", ModelPartBuilder.create()
-                .uv(16, 16).cuboid(-3.9F, 0.0F, -2.1F, 8.0F, 12.0F, 4.0F, new Dilation(0.75F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+                .uv(16, 16).cuboid(-3.9F, 0.0F, -2.1F, 8.0F, 12.0F, 4.0F, new Dilation(0.75F)),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
         modelPartData.addChild("right_arm", ModelPartBuilder.create()
-                .uv(40, 16).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+                .uv(40, 16).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)),
+                ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
 
         modelPartData.addChild("left_arm", ModelPartBuilder.create()
-                .uv(40, 16).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)).mirrored(false), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+                .uv(40, 16).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)).mirrored(false),
+                ModelTransform.pivot(5.0F, 2.0F, 0.0F));
 
         modelPartData.addChild("right_leg", ModelPartBuilder.create()
-                .uv(0, 16).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)), ModelTransform.pivot(-1.9F, 12.0F, 0.0F));
+                .uv(0, 16).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)),
+                ModelTransform.pivot(-1.9F, 12.0F, 0.0F));
 
         modelPartData.addChild("left_leg", ModelPartBuilder.create()
-                .uv(0, 16).mirrored().cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)).mirrored(false), ModelTransform.pivot(1.9F, 12.0F, 0.0F));
+                .uv(0, 16).mirrored().cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.7F)).mirrored(false),
+                ModelTransform.pivot(1.9F, 12.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 32);
+    }
+
+    @Override
+    public void setAngles(T livingEntity, float f, float g, float h, float i, float j) {
+        if (!(livingEntity instanceof ArmorStandEntity entityIn)) {
+            if (!(livingEntity instanceof LivingEntity entityIn)) {
+            super.setAngles(livingEntity, f, g, h, i, j);
+            return;
+        }
+
+        this.head.yaw = ((float) Math.PI / 180F) * entityIn.getHeadYaw();
+
+        this.head.setPivot(0.0F, 1.0F, 0.0F);
+        this.body.yaw = entityIn.bodyYaw;
+
+        this.leftArm.yaw = ((float) Math.PI / 180F) * entityIn.getLeftArmRotation().getYaw();
+        this.leftArm.pitch = ((float) Math.PI / 180F) * entityIn.getLeftArmRotation().getPitch();
+        this.leftArm.roll = ((float) Math.PI / 180F) * entityIn.getLeftArmRotation().getRoll();
+        this.rightArm.yaw = ((float) Math.PI / 180F) * entityIn.getRightArmRotation().getYaw();
+        this.rightArm.pitch = ((float) Math.PI / 180F) * entityIn.getRightArmRotation().getPitch();
+        this.rightArm.roll = ((float) Math.PI / 180F) * entityIn.getRightArmRotation().getRoll();
+
+        this.leftLeg.yaw = ((float) Math.PI / 180F) * entityIn.getLeftLegRotation().getYaw();
+        this.leftLeg.pitch = ((float) Math.PI / 180F) * entityIn.getLeftLegRotation().getPitch();
+        this.leftLeg.roll = ((float) Math.PI / 180F) * entityIn.getLeftLegRotation().getRoll();
+        this.rightLeg.yaw = ((float) Math.PI / 180F) * entityIn.getRightLegRotation().getYaw();
+        this.rightLeg.pitch = ((float) Math.PI / 180F) * entityIn.getRightLegRotation().getPitch();
+        this.rightLeg.roll = ((float) Math.PI / 180F) * entityIn.getRightLegRotation().getRoll();
+
+        this.leftLeg.setPivot(1.9F, 11.0F, 0.0F);
+        this.rightLeg.setPivot(-1.9F, 11.0F, 0.0F);
+        this.hat.copyTransform(this.head);
     }
 }
