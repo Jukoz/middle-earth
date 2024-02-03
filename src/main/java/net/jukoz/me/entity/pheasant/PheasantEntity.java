86746f7041913aa3cda72da4f6e02f8f5a83a53e
TomSchlom <thomas.huber04@gmail.com>
2024-02-03 11:03:03 +0100

    Improve Pheasant flight

--- src/main/java/net/jukoz/me/entity/pheasant/PheasantEntity.java
+++ src/main/java/net/jukoz/me/entity/pheasant/PheasantEntity.java
@@ -2,7 +2,10 @@ package net.jukoz.me.entity.pheasant;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.*;
+import net.minecraft.entity.ai.NoPenaltyTargeting;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.Path;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -167,24 +170,65 @@ public class PheasantEntity extends AnimalEntity {
         PheasantEntity pheasant = PheasantEntity.this;
         PlayerEntity player;
         Vec3d vec3d = Vec3d.ZERO;
+        Vec3d fleeDir;
+        @Nullable
+        Path fleePath;
+        final EntityNavigation fleeingEntityNavigation;
+        boolean flying;
 
         public FleeWhenStartledGoal() {
             this.setControls(EnumSet.of(Control.MOVE));
+            this.fleeingEntityNavigation = pheasant.getNavigation();
         }
 
         @Override
         public boolean canStart() {
             player = pheasant.getWorld().getClosestPlayer(pheasant, 7.0d);
+            if(player == null) {
+                return false;
+            }
+
+            vec3d = NoPenaltyTargeting.findFrom(this.pheasant, 4, 3, this.player.getPos());
+            if (vec3d == null) {
+                return false;
+            }
+            if (this.player.squaredDistanceTo(vec3d.x, vec3d.y, vec3d.z) < this.player.squaredDistanceTo(this.pheasant)) {
+                return false;
+            }
+
+            this.fleePath = this.fleeingEntityNavigation.findPathTo(vec3d.x, vec3d.y, vec3d.z, 0);
+            if(this.fleePath == null) {
+                return false;
+            }
 
             return player != null && player.isSprinting() && pheasant.isOnGround();
         }
 
         @Override
         public void start() {
-            vec3d = pheasant.getPos().subtract(player.getPos()).multiply(1, 0, 1).normalize();
+            fleeDir = vec3d.subtract(pheasant.getPos()).multiply(1,0,1).normalize();
+            this.flying = false;
+            this.fleeingEntityNavigation.startMovingAlong(this.fleePath, 1.6d);
+        }
 
-            pheasant.setYaw((float) Math.toDegrees(Math.atan2(-vec3d.x, vec3d.z)));
-            pheasant.setVelocity(vec3d.multiply(1.8,0,1.8).add(0,0.4,0));
+        @Override
+        public void tick() {
+            if(this.fleeingEntityNavigation.isIdle() && !this.flying) {
+                this.pheasant.setVelocity(fleeDir.multiply(1.7, 0, 1.7).add(0, 0.5, 0));
+                this.pheasant.setYaw((float) Math.toDegrees(Math.atan2(-fleeDir.x, fleeDir.z)));
+                this.flying = true;
+            }
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            return !this.flying;
+        }
+
+        @Override
+        public void stop() {
+            this.player = null;
+            this.flying = false;
         }
     }
 
