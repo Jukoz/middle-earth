68daa69aea755cfc5bdc7d10f308e7f2be3eff77
SlooshyBoi <robin.hugovv@gmail.com>
2024-02-16 01:39:46 +0100

    Clean ArmorRenderer

--- src/main/java/net/jukoz/me/client/renderer/ModArmorRenderer.java
+++ src/main/java/net/jukoz/me/client/renderer/ModArmorRenderer.java
@@ -31,9 +31,7 @@ import java.awt.*;
 
 public class ModArmorRenderer implements ArmorRenderer {
     private  Identifier ARMOR_LAYER_0;
-    private  Identifier ARMOR_LAYER_0_OVERLAY;
     private  Identifier ARMOR_LAYER_1;
-    private  Identifier ARMOR_LAYER_1_OVERLAY;
     private  Identifier ARMOR_LAYER_2;
     private  Identifier ARMOR_LAYER_CAPE;
     private  Identifier ARMOR_LAYER_HOOD;
@@ -57,9 +55,7 @@ public class ModArmorRenderer implements ArmorRenderer {
         helmetModel = customHelmetModel;
         chestplateModel = customChestplateModel;
         ARMOR_LAYER_0 = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_layer_0.png");
-        ARMOR_LAYER_0_OVERLAY = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_layer_0_overlay.png");
         ARMOR_LAYER_1 = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_layer_1.png");
-        ARMOR_LAYER_1_OVERLAY = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_layer_1_overlay.png");
         ARMOR_LAYER_2 = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_layer_2.png");
         ARMOR_LAYER_CAPE = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_cape.png");
         ARMOR_LAYER_HOOD = new Identifier(MiddleEarth.MOD_ID, "textures/models/armor/" + armorName + "_hood.png");
@@ -81,162 +77,95 @@ public class ModArmorRenderer implements ArmorRenderer {
             hoodModel = new CloakHoodModel<>(MinecraftClient.getInstance().getEntityModelLoader().getModelPart(MiddleEarthClient.HOOD_MODEL_LAYER));
         }
 
-        if(!dyeable) {
-            if (slot == EquipmentSlot.HEAD) {
-                if(hasVanillaArmorModel){
-                    contextModel.copyBipedStateTo(armorModel);
-                    armorModel.setVisible(false);
-                    armorModel.head.visible = true;
-                    armorModel.hat.visible = true;
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
-                }
-                if (helmetModel != null) {
-                    contextModel.copyBipedStateTo(helmetModel);
-                    helmetModel.setVisible(false);
-                    helmetModel.head.visible = true;
-                    helmetModel.setAngles(entity, entity.limbAnimator.getPos(), entity.limbAnimator.getSpeed(),(float)entity.age + MinecraftClient.getInstance().getTickDelta(), contextModel.head.yaw, contextModel.head.pitch);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, helmetModel, helmetModel.HELMET_ADDON_TEXTURE);
-                }
-                if(hasHood){
-                    contextModel.copyBipedStateTo(hoodModel);
-                    hoodModel.setVisible(false);
-                    hoodModel.hat.visible = true;
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, hoodModel, ARMOR_LAYER_HOOD);
-                }
-            } else if (slot == EquipmentSlot.CHEST) {
-                if(hasInnerLayer) {
-                    contextModel.copyBipedStateTo(innerArmorModel);
-                    innerArmorModel.setVisible(false);
-                    innerArmorModel.body.visible = true;
-                    innerArmorModel.rightArm.visible = true;
-                    innerArmorModel.leftArm.visible = true;
-                    innerArmorModel.rightLeg.visible = true;
-                    innerArmorModel.leftLeg.visible = true;
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0);
-                }
+        if (slot == EquipmentSlot.HEAD) {
+            if(hasVanillaArmorModel){
+                contextModel.copyBipedStateTo(armorModel);
+                armorModel.setVisible(false);
+                armorModel.head.visible = true;
+                armorModel.hat.visible = true;
+                renderArmor(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1, this.dyeable);
+            }
+            if (helmetModel != null) {
+                contextModel.copyBipedStateTo(helmetModel);
+                helmetModel.setVisible(false);
+                helmetModel.head.visible = true;
+                helmetModel.setAngles(entity, entity.limbAnimator.getPos(), entity.limbAnimator.getSpeed(),(float)entity.age + MinecraftClient.getInstance().getTickDelta(), contextModel.head.yaw, contextModel.head.pitch);
+                renderArmor(matrices, vertexConsumers, light, stack, helmetModel, helmetModel.HELMET_ADDON_TEXTURE, this.dyeable);
+            }
+            if(hasHood){
+                contextModel.copyBipedStateTo(hoodModel);
+                hoodModel.setVisible(false);
+                hoodModel.hat.visible = true;
+                renderArmor(matrices, vertexConsumers, light, stack, hoodModel, ARMOR_LAYER_HOOD, this.dyeable);
+            }
+        } else if (slot == EquipmentSlot.CHEST) {
+            if(hasInnerLayer) {
+                contextModel.copyBipedStateTo(innerArmorModel);
+                innerArmorModel.setVisible(false);
+                innerArmorModel.body.visible = true;
+                innerArmorModel.rightArm.visible = true;
+                innerArmorModel.leftArm.visible = true;
+                innerArmorModel.rightLeg.visible = true;
+                innerArmorModel.leftLeg.visible = true;
+                renderArmor(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0, this.dyeable);
+            }
+
+            if(hasVanillaArmorModel) {
                 contextModel.copyBipedStateTo(armorModel);
                 armorModel.setVisible(false);
                 armorModel.body.visible = true;
                 armorModel.rightArm.visible = true;
                 armorModel.leftArm.visible = true;
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
-
-                if(this.chestplateModel != null) {
-                    contextModel.copyBipedStateTo(chestplateModel);
-                    chestplateModel.setVisible(false);
-                    chestplateModel.body.visible = true;
-                    chestplateModel.rightArm.visible = true;
-                    chestplateModel.leftArm.visible = true;
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, chestplateModel, chestplateModel.CHESTPLATE_ADDON_TEXTURE);
-                }
-
-                if (this.hasCape) {
-                    contextModel.copyBipedStateTo(capeModel);
-                    capeModel.setVisible(false);
-                    capeModel.body.visible = true;
-                    capeModel.rightArm.visible = true;
-                    capeModel.leftArm.visible = true;
-                    capeModel.setAngles(entity, entity.limbAnimator.getPos(),entity.limbAnimator.getSpeed(),(float)entity.age + MinecraftClient.getInstance().getTickDelta(), contextModel.head.yaw, contextModel.head.roll);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, capeModel, ARMOR_LAYER_CAPE);
-                }
-            } else if (slot == EquipmentSlot.LEGS) {
-                if(hasInnerLayer) {
-                    contextModel.copyBipedStateTo(innerArmorModel);
-                    innerArmorModel.setVisible(false);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0);
-                }
-                contextModel.copyBipedStateTo(legArmorModel);
-                legArmorModel.setVisible(false);
-                legArmorModel.body.visible = true;
-                legArmorModel.rightLeg.visible = true;
-                legArmorModel.leftLeg.visible = true;
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, legArmorModel, ARMOR_LAYER_2);
-            } else if (slot == EquipmentSlot.FEET) {
-                contextModel.copyBipedStateTo(armorModel);
-                armorModel.setVisible(false);
-                armorModel.rightLeg.visible = true;
-                armorModel.leftLeg.visible = true;
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
+                renderArmor(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1, this.dyeable);
             }
+
+            if(this.chestplateModel != null) {
+                contextModel.copyBipedStateTo(chestplateModel);
+                chestplateModel.setVisible(false);
+                chestplateModel.body.visible = true;
+                chestplateModel.rightArm.visible = true;
+                chestplateModel.leftArm.visible = true;
+                renderArmor(matrices, vertexConsumers, light, stack, chestplateModel, chestplateModel.CHESTPLATE_ADDON_TEXTURE, this.dyeable);
+            }
+
+            if (this.hasCape) {
+                contextModel.copyBipedStateTo(capeModel);
+                capeModel.setVisible(false);
+                capeModel.body.visible = true;
+                capeModel.rightArm.visible = true;
+                capeModel.leftArm.visible = true;
+                capeModel.setAngles(entity, entity.limbAnimator.getPos(),entity.limbAnimator.getSpeed(),(float)entity.age + MinecraftClient.getInstance().getTickDelta(), contextModel.head.yaw, contextModel.head.roll);
+                renderArmor(matrices, vertexConsumers, light, stack, capeModel, ARMOR_LAYER_CAPE, this.dyeable);
+            }
+        } else if (slot == EquipmentSlot.LEGS) {
+            if(hasInnerLayer) {
+                contextModel.copyBipedStateTo(innerArmorModel);
+                innerArmorModel.setVisible(false);
+                renderArmor(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0, this.dyeable);
+            }
+            contextModel.copyBipedStateTo(legArmorModel);
+            legArmorModel.setVisible(false);
+            legArmorModel.body.visible = true;
+            legArmorModel.rightLeg.visible = true;
+            legArmorModel.leftLeg.visible = true;
+            renderArmor(matrices, vertexConsumers, light, stack, legArmorModel, ARMOR_LAYER_2, this.dyeable);
+        } else if (slot == EquipmentSlot.FEET) {
+            contextModel.copyBipedStateTo(armorModel);
+            armorModel.setVisible(false);
+            armorModel.rightLeg.visible = true;
+            armorModel.leftLeg.visible = true;
+            renderArmor(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1, this.dyeable);
+        }
+    }
+
+
+    static void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ItemStack stack, Model model, Identifier texture, boolean dyeable){
+        if(dyeable){
+            renderDyeable(matrices, vertexConsumers, light, stack, model, texture);
+            renderDyeable(matrices, vertexConsumers, light, stack, model, new Identifier(MiddleEarth.MOD_ID,
+                    texture.getPath().replaceAll(".png", "_overlay.png")));
         } else {
-            if (slot == EquipmentSlot.HEAD) {
-                if(hasVanillaArmorModel) {
-                    contextModel.copyBipedStateTo(armorModel);
-                    armorModel.setVisible(false);
-                    armorModel.head.visible = true;
-                    armorModel.hat.visible = true;
-                    renderDyeable(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1_OVERLAY);
-                }
-                if (helmetModel != null) {
-                    contextModel.copyBipedStateTo(helmetModel);
-                    helmetModel.setVisible(false);
-                    helmetModel.head.visible = true;
-                    helmetModel.setAngles(entity, entity.limbAnimator.getPos(), entity.limbAnimator.getSpeed(),(float)entity.age + MinecraftClient.getInstance().getTickDelta(), contextModel.head.yaw, contextModel.head.pitch);
-                    renderDyeable(matrices, vertexConsumers, light, stack, helmetModel, helmetModel.HELMET_ADDON_TEXTURE);
-                }
-                if(this.hasHood){
-                    contextModel.copyBipedStateTo(hoodModel);
-                    hoodModel.setVisible(false);
-                    hoodModel.hat.visible = true;
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, hoodModel, ARMOR_LAYER_HOOD);
-                }
-            } else if (slot == EquipmentSlot.CHEST) {
-                if(hasInnerLayer) {
-                    contextModel.copyBipedStateTo(innerArmorModel);
-                    innerArmorModel.setVisible(false);
-                    innerArmorModel.body.visible = true;
-                    innerArmorModel.rightArm.visible = true;
-                    innerArmorModel.leftArm.visible = true;
-                    innerArmorModel.rightLeg.visible = true;
-                    innerArmorModel.leftLeg.visible = true;
-                    renderDyeable(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0_OVERLAY);
-                }
-                if(hasVanillaArmorModel) {
-                    contextModel.copyBipedStateTo(armorModel);
-                    armorModel.setVisible(false);
-                    armorModel.body.visible = true;
-                    armorModel.rightArm.visible = true;
-                    armorModel.leftArm.visible = true;
-                    renderDyeable(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
-                    ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1_OVERLAY);
-                }
-
-                if(this.chestplateModel != null) {
-                    contextModel.copyBipedStateTo(chestplateModel);
-                    chestplateModel.setVisible(false);
-                    chestplateModel.body.visible = true;
-                    chestplateModel.rightArm.visible = true;
-                    chestplateModel.leftArm.visible = true;
-                    renderDyeable(matrices, vertexConsumers, light, stack, chestplateModel, chestplateModel.CHESTPLATE_ADDON_TEXTURE);
-                }
-
-                if (this.hasCape) {
-                    contextModel.copyBipedStateTo(capeModel);
-                    capeModel.setVisible(false);
-                    capeModel.body.visible = true;
-                    renderDyeable(matrices, vertexConsumers, light, stack, capeModel, ARMOR_LAYER_CAPE);
-                }
-            } else if (slot == EquipmentSlot.LEGS) {
-                if(hasInnerLayer) {
-                    contextModel.copyBipedStateTo(innerArmorModel);
-                    innerArmorModel.setVisible(false);
-                    renderDyeable(matrices, vertexConsumers, light, stack, innerArmorModel, ARMOR_LAYER_0);
-                }
-                contextModel.copyBipedStateTo(legArmorModel);
-                legArmorModel.setVisible(false);
-                legArmorModel.body.visible = true;
-                legArmorModel.rightLeg.visible = true;
-                legArmorModel.leftLeg.visible = true;
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, legArmorModel, ARMOR_LAYER_2);
-            } else if (slot == EquipmentSlot.FEET) {
-                contextModel.copyBipedStateTo(armorModel);
-                armorModel.setVisible(false);
-                armorModel.rightLeg.visible = true;
-                armorModel.leftLeg.visible = true;
-                renderDyeable(matrices, vertexConsumers, light, stack, armorModel, ARMOR_LAYER_1);
-            }
+            ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, model, texture);
         }
     }
 
