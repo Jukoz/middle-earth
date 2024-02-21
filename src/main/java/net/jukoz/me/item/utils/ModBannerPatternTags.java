16bd778b58c98d01ab5866b211c944fcaf4f0b8c
SlooshyBoi <robin.hugovv@gmail.com>
2024-02-21 23:22:11 +0100

    Fix TagKey registration

--- src/main/java/net/jukoz/me/item/utils/ModBannerPatternTags.java
+++ src/main/java/net/jukoz/me/item/utils/ModBannerPatternTags.java
@@ -7,16 +7,16 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModBannerPatternTags {
-    public static final TagKey<BannerPattern> GONDOR_PATTERN_ITEM = of("pattern_item/gondor");
-    public static final TagKey<BannerPattern> ROHAN_PATTERN_ITEM = of("pattern_item/rohan");
-    public static final TagKey<BannerPattern> LONGBEARD_PATTERN_ITEM = of("pattern_item/longbeard");
-    public static final TagKey<BannerPattern> LOTHLORIEN_PATTERN_ITEM = of("pattern_item/lothlorien");
-    public static final TagKey<BannerPattern> MORDOR_GREAT_EYE_PATTERN_ITEM = of("pattern_item/mordor_great_eye");
-    public static final TagKey<BannerPattern> MORDOR_EYE_PATTERN_ITEM = of("pattern_item/mordor_eye");
-    public static final TagKey<BannerPattern> MISTY_ORCS_EYE_PATTERN_ITEM = of("pattern_item/misty_orcs_eye");
-    public static final TagKey<BannerPattern> MISTY_ORCS_PEAKS_PATTERN_ITEM = of("pattern_item/misty_orcs_peaks");
+    public static final TagKey<BannerPattern> GONDOR_PATTERN_ITEM = of("gondor");
+    public static final TagKey<BannerPattern> ROHAN_PATTERN_ITEM = of("rohan");
+    public static final TagKey<BannerPattern> LONGBEARD_PATTERN_ITEM = of("longbeard");
+    public static final TagKey<BannerPattern> LOTHLORIEN_PATTERN_ITEM = of("lothlorien");
+    public static final TagKey<BannerPattern> MORDOR_GREAT_EYE_PATTERN_ITEM = of("mordor_great_eye");
+    public static final TagKey<BannerPattern> MORDOR_EYE_PATTERN_ITEM = of("mordor_eye");
+    public static final TagKey<BannerPattern> MISTY_ORCS_EYE_PATTERN_ITEM = of("misty_orcs_eye");
+    public static final TagKey<BannerPattern> MISTY_ORCS_PEAKS_PATTERN_ITEM = of("misty_orcs_peaks");
 
     private static TagKey<BannerPattern> of(String name) {
-        return TagKey.of(RegistryKeys.BANNER_PATTERN, new Identifier(name));
+        return TagKey.of(RegistryKeys.BANNER_PATTERN, new Identifier("pattern_item/" + name));
     }
 }
