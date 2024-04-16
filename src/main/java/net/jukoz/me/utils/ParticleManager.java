09ab3b157da8c0dc74b2088ba8a5ca5edf8ff1aa
SlooshyBoi <robin.hugovv@gmail.com>
2024-04-16 15:59:18 +0200

    Fix Particles

--- src/main/java/net/jukoz/me/utils/ParticleManager.java
+++ src/main/java/net/jukoz/me/utils/ParticleManager.java
@@ -1,7 +1,6 @@
 package net.jukoz.me.utils;
 
-import net.minecraft.particle.DefaultParticleType;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
 
@@ -10,15 +9,15 @@ public class ParticleManager {
     private static final float PARTICLE_VELOCITY = 0.25f;
     private static final int PARTICLE_QUANTITY = 15;
 
-    public static void createParticles(World world, Position pos, DefaultParticleType particleType) {
+    public static void createParticles(World world, Position pos, SimpleParticleType particleType) {
         createParticles(world, pos, particleType, PARTICLE_QUANTITY, PARTICLE_VELOCITY, RANDOM_PARTICLE_RANGE);
     }
 
-    public static void createParticles(World world, Position pos, DefaultParticleType particleType, int quantity) {
+    public static void createParticles(World world, Position pos, SimpleParticleType particleType, int quantity) {
         createParticles(world, pos, particleType, quantity, PARTICLE_VELOCITY, RANDOM_PARTICLE_RANGE);
     }
 
-    public static void createParticles(World world, Position pos, DefaultParticleType particleType, int quantity, float velocity, float range) {
+    public static void createParticles(World world, Position pos, SimpleParticleType particleType, int quantity, float velocity, float range) {
         for (int i = 0; i < quantity; i++) {
             world.addParticle(particleType,
                     pos.getX() + range * (-0.5f + Math.random()),
