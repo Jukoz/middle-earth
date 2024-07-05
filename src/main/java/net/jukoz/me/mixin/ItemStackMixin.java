26fefda28028bca25c3f4d5f5733d62a6c517a8a
Jukoz <olegptk12@gmail.com>
2024-07-04 20:51:09 -0400

    Refactors tooltip mixin

--- src/main/java/net/jukoz/me/mixin/ItemStackMixin.java
+++ src/main/java/net/jukoz/me/mixin/ItemStackMixin.java
@@ -2,19 +2,28 @@ package net.jukoz.me.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.jukoz.me.item.ModDataComponentTypes;
 import net.minecraft.client.item.TooltipType;
 import net.minecraft.component.DataComponentType;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TooltipAppender;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
@@ -24,15 +33,11 @@ public abstract class ItemStackMixin {
 
     @Shadow public abstract Item getItem();
 
-    @WrapOperation(method = "getTooltip",
-            at = @At(value = "INVOKE",
-                    ordinal = 0,
-                    target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/DataComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/client/item/TooltipType;)V"),
-            slice = @Slice(from = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/DataComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/client/item/TooltipType;)V"),
-                            to = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/DataComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/client/item/TooltipType;)V")))
+    @Inject(method = "getTooltip", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/DataComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/client/item/TooltipType;)V",
+            ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
 
-    private void addCustomTooltips(ItemStack instance, DataComponentType<?> componentType, Item.TooltipContext context, Consumer<Text> textConsumer, TooltipType type, Operation<Void> original){
-        this.appendTooltip(ModDataComponentTypes.TEMPERATURE_DATA, context, textConsumer, type);
-        this.appendTooltip(DataComponentTypes.TRIM, context, textConsumer, type);
+    private void addCustomTooltips(Item.TooltipContext context, @Nullable PlayerEntity player, TooltipType type, CallbackInfoReturnable<List<Text>> cir, @Local Consumer<Text> consumer){
+        this.appendTooltip(ModDataComponentTypes.TEMPERATURE_DATA, context, consumer, type);
     }
 }
