a135fd064aa5bc1cc4297384bbd89a28969fa01d
Jukoz <olegptk12@gmail.com>
2024-07-08 15:37:56 -0400

    Fixes mixins

--- src/main/java/net/jukoz/me/mixin/ItemStackMixin.java
+++ src/main/java/net/jukoz/me/mixin/ItemStackMixin.java
@@ -1,9 +1,10 @@
 package net.jukoz.me.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.jukoz.me.item.ModDataComponentTypes;
 import net.minecraft.component.ComponentType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipAppender;
@@ -12,11 +13,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
@@ -26,11 +23,11 @@ public abstract class ItemStackMixin {
 
     @Shadow public abstract Item getItem();
 
-    @Inject(method = "getTooltip", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/DataComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/client/item/TooltipType;)V",
-            ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+    @WrapOperation(method = "getTooltip", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/ComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/item/tooltip/TooltipType;)V",
+            ordinal = 0))
 
-    private void addCustomTooltips(Item.TooltipContext context, PlayerEntity player, TooltipType type, CallbackInfoReturnable<List<Text>> cir, @Local Consumer<Text> consumer){
+    private void addCustomTooltips(ItemStack instance, ComponentType componentType, Item.TooltipContext context, Consumer<Text> textConsumer, TooltipType type, Operation<Void> original, @Local Consumer<Text> consumer){
         this.appendTooltip(ModDataComponentTypes.TEMPERATURE_DATA, context, consumer, type);
     }
 }
