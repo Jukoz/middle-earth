d6db8e8cef025798ca84ffd9f5d2614daf64518b
SlooshyBoi <robin.hugovv@gmail.com>
2024-07-17 01:10:09 +0200

    Update weapon types stats

--- src/main/java/net/jukoz/me/item/utils/ModWeaponTypes.java
+++ src/main/java/net/jukoz/me/item/utils/ModWeaponTypes.java
@@ -2,11 +2,11 @@ package net.jukoz.me.item.utils;
 
 public enum ModWeaponTypes {
 
-    SWORD("sword", 3, -2.3f, 0.0F, false),
-    AXE("axe", 6, -3.1f, 0.0F, false),
-    DAGGER("dagger", 2, -0.7f, -1.0F, false),
-    SPEAR("spear", 2, -2.5f, 1.0F, false),
-    LONGSWORD("longsword", 3, -2.5f, 1.0F, true),
+    SWORD("sword", 4, -2.4f, 0.0F, false),
+    AXE("axe", 6, -3.0f, 0.0F, false),
+    DAGGER("dagger", 1, -0.7f, -1.0F, false),
+    SPEAR("spear", 3, -2.5f, 1.0F, false),
+    LONGSWORD("longsword", 5, -2.6f, 1.0F, true),
     ;
 
     public final String name;
