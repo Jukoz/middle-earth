c047abf5e64674025e69ca27f5a0d9f9a2075a9c
Jukoz <olegptk12@gmail.com>
2024-07-30 15:18:19 -0400

    Adds luminance to forge

--- src/main/java/net/jukoz/me/block/special/forge/ForgeBlockEntityRenderer.java
+++ src/main/java/net/jukoz/me/block/special/forge/ForgeBlockEntityRenderer.java
@@ -9,12 +9,26 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector2f;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.*;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ForgeBlockEntityRenderer implements BlockEntityRenderer<ForgeBlockEntity> {
+    private final float OFFSET = 0.25f;
+    private final List<Vector2f> POSITIONS = new ArrayList<>(Arrays.asList(
+            new Vector2f(-OFFSET,-OFFSET),
+            new Vector2f(OFFSET, -OFFSET),
+            new Vector2f(-OFFSET,OFFSET),
+            new Vector2f(OFFSET, OFFSET)));
+
     public ForgeBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
     }
 
@@ -22,11 +36,16 @@ public class ForgeBlockEntityRenderer implements BlockEntityRenderer<ForgeBlockE
     public void render(ForgeBlockEntity entity, float tickDelta, MatrixStack matrices,
                        VertexConsumerProvider vertexConsumers, int light, int overlay) {
         ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-        //BlockModelRenderer blockRenderer = MinecraftClient.getInstance().getBlockRenderManager().getModelRenderer();
-        ItemStack output = entity.getRenderStack();
+
+        List<ItemStack> inputs = new ArrayList<>();
+        Inventory inventory = (Inventory) entity;
+        inputs.add(inventory.getStack(1));
+        inputs.add(inventory.getStack(2));
+        inputs.add(inventory.getStack(3));
+        inputs.add(inventory.getStack(4));
 
         matrices.push();
-        matrices.translate(0.5f, 0.76f, 0.5f);
+        matrices.translate(0.5f, 1.15f, 0.5f);
         matrices.scale(0.5f, 0.5f, 0.5f);
 
         matrices.multiply(RotationAxis.POSITIVE_X.rotation((float) Math.toRadians(-90)));
@@ -45,9 +64,16 @@ public class ForgeBlockEntityRenderer implements BlockEntityRenderer<ForgeBlockE
         //    blockRenderer.render(matrices.peek(), vertexConsumers.getBuffer(TexturedRenderLayers.getEntitySolid()), null, bakedModel,
         //            1.0f, 1.0f, 1.0f, currentLight, OverlayTexture.DEFAULT_UV);
         //}
-        if(!output.isEmpty()) {
-            itemRenderer.renderItem(output, ModelTransformationMode.GUI, currentLight, OverlayTexture.DEFAULT_UV,
-                    matrices, vertexConsumers, entity.getWorld(), 1);
+        if(!inputs.isEmpty() && inputs.size() <= POSITIONS.size()) {
+            for(int i = 0; i < inputs.size(); i++) {
+                Vector2f position = POSITIONS.get(i);
+                matrices.translate(position.getX(), 0, position.getY());
+
+                itemRenderer.renderItem(inputs.get(i), ModelTransformationMode.FIXED, currentLight, OverlayTexture.DEFAULT_UV,
+                        matrices, vertexConsumers, entity.getWorld(), 1);
+
+                matrices.translate(-position.getX(), 0, -position.getY());
+            }
         }
 
         matrices.pop();
