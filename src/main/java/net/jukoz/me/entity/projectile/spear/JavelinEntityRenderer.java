dbdd20a968a5cb116ed1d63901159fc2b6e604bf
Jukoz <olegptk12@gmail.com>
2024-09-24 19:42:26 -0400

    Add throwing spears

--- src/main/java/net/jukoz/me/entity/projectile/spear/JavelinEntityRenderer.java
+++ src/main/java/net/jukoz/me/entity/projectile/spear/JavelinEntityRenderer.java
@@ -12,13 +12,14 @@ import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.FlyingItemEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 
 @Environment(EnvType.CLIENT)
-public class JavelinEntityRenderer<T extends Entity & FlyingItemEntity> extends EntityRenderer<T> {
+public class JavelinEntityRenderer<T extends SpearEntity> extends EntityRenderer<T> {
     private static final float MIN_DISTANCE = 12.25F;
     private static final float SCALE = 1.5F;
     private final ItemRenderer itemRenderer;
@@ -50,12 +51,13 @@ public class JavelinEntityRenderer<T extends Entity & FlyingItemEntity> extends 
         matrices.scale(scale, scale, scale);
         matrices.translate(0.0D, -0.2D, 0.0D);
 
-        this.itemRenderer.renderItem(entity.getStack(), ModelTransformationMode.THIRD_PERSON_RIGHT_HAND, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), entity.getId());
+        this.itemRenderer.renderItem(entity.getItemStack(), ModelTransformationMode.THIRD_PERSON_RIGHT_HAND, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), entity.getId());
         matrices.pop();
         super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
     }
 
-    public Identifier getTexture(Entity entity) {
+    @Override
+    public Identifier getTexture(T entity) {
         return SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE;
     }
 }
--- src/main/java/net/jukoz/me/entity/projectile/spear/SpearEntity.java
+++ src/main/java/net/jukoz/me/entity/projectile/spear/SpearEntity.java
@@ -3,49 +3,39 @@ package net.jukoz.me.entity.projectile.spear;
 import net.jukoz.me.entity.ModEntities;
 import net.jukoz.me.entity.hobbits.shire.ShireHobbitEntity;
 import net.jukoz.me.item.ModWeaponItems;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.EntityEffectParticleEffect;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class SpearEntity extends ThrownItemEntity {
-    private Item item;
+public class SpearEntity extends PersistentProjectileEntity {
     private float damage;
 
     public SpearEntity(EntityType<? extends SpearEntity> entityType, World world) {
         super(entityType, world);
-        this.item = ModWeaponItems.GONDORIAN_SPEAR;
     }
 
-    public SpearEntity(World world, Item item, LivingEntity owner, float dmg) {
-        super(ModEntities.SPEAR, owner, world);
-        this.item = item;
+    public SpearEntity(World world, ItemStack itemStack, LivingEntity owner, float dmg) {
+        super(ModEntities.SPEAR, owner, world, itemStack, null);
         this.damage = dmg;
     }
 
-    protected Item getDefaultItem() {
-        return item;
-    }
-
-    private ParticleEffect getParticleParameters() {
-        return new ItemStackParticleEffect(ParticleTypes.ITEM, new ItemStack(item));
-    }
-
-    public void handleStatus(byte status) {
-        if (status == 3) {
-            ParticleEffect particleEffect = this.getParticleParameters();
-            for(int i = 0; i < 8; ++i) {
-                getWorld().addParticle(particleEffect, this.getX(), this.getY(), this.getZ(), 0.0, 0.0, 0.0);
-            }
-        }
+    protected SoundEvent getHitSound() {
+        return SoundEvents.ITEM_TRIDENT_HIT;
     }
 
     protected void onEntityHit(EntityHitResult entityHitResult) {
@@ -55,11 +45,17 @@ public class SpearEntity extends ThrownItemEntity {
         entity.damage(this.getDamageSources().thrown(this, this.getOwner()), this.damage);
     }
 
-    protected void onCollision(HitResult hitResult) {
-        super.onCollision(hitResult);
-        if (!getWorld().isClient) {
-            getWorld().sendEntityStatus(this, (byte)3);
-            this.discard();
-        }
+    @Override
+    protected ItemStack getDefaultItemStack() {
+        return ModWeaponItems.WOODEN_SPEAR.getDefaultStack();
+    }
+
+    protected float getDragInWater() {
+        return 0.8f;
+    }
+
+    @Override
+    public boolean shouldRender(double cameraX, double cameraY, double cameraZ) {
+        return true;
     }
 }
