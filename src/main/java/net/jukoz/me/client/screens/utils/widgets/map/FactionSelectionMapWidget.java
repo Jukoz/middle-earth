83b6c2f7b7d7d226bfcc414c0f4e92c497a50796
krahab <remygarenc@gmail.com>
2024-09-30 19:39:02 -0400

    Add map marker group up

--- src/main/java/net/jukoz/me/client/screens/utils/widgets/map/FactionSelectionMapWidget.java
+++ src/main/java/net/jukoz/me/client/screens/utils/widgets/map/FactionSelectionMapWidget.java
@@ -8,7 +8,10 @@ import net.jukoz.me.utils.LoggerUtil;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import org.joml.Vector2d;
+import org.joml.Vector2i;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FactionSelectionMapWidget extends MapWidget {
@@ -46,22 +49,62 @@ public class FactionSelectionMapWidget extends MapWidget {
         LoggerUtil.logDebugMsg("Clicked on spawn #" + index);
     }
 
+    protected double getMarkerGroupUpRadius(){
+        return 15;
+    }
     @Override
     protected void draw(DrawContext context, int startX, int startY) {
         super.draw(context, startX, startY);
         SpawnDataHandler handler = controller.getCurrentSpawnDataHandler();
         if(handler == null || handler.getSpawnList() == null) return;
         List<SpawnData> spawns = handler.getSpawnList();
+        HashMap<Integer, List<Vector2i>> uniqueIndexes = new HashMap<>();
         for(int i = 0; i < spawns.size(); i++){
             SpawnData spawnData = spawns.get(i);
             Vector2d coordinates = new Vector2d(spawnData.getCoordinates().getX(), spawnData.getCoordinates().getZ());
+            MapMarkerWidget mapMarker = spawnMapMarkers[i];
             if(spawnData.isDynamic()){
-                spawnMapMarkers[i].setType(MapMarkerType.DYNAMIC_SPAWN);
-                spawnMapMarkers[i].drawFromMapPosition(context, this, coordinates);
+                mapMarker.setType(MapMarkerType.DYNAMIC_SPAWN);
+                mapMarker.computeFromMapPosition(this, coordinates);
             } else {
-                spawnMapMarkers[i].setType(MapMarkerType.CUSTOM_SPAWN);
-                spawnMapMarkers[i].drawFromWorldPosition(context, this, coordinates);
+                mapMarker.setType(MapMarkerType.CUSTOM_SPAWN);
+                mapMarker.computeFromWorldPosition(this, coordinates);
+            }
+            Vector2i currentCenterCoordinate = mapMarker.getCenterCoordinates();
+            boolean isSeperate = true;
+            int currentUniqueIndex = 0;
+            for(int j = 0; j < uniqueIndexes.size() && isSeperate; j++){
+                currentUniqueIndex = uniqueIndexes.keySet().stream().toList().get(j);
+                List<Vector2i> currentList = uniqueIndexes.get(currentUniqueIndex);
+                for (Vector2i vector2i : currentList) {
+                    double distance = currentCenterCoordinate.distance(vector2i);
+                    if (distance < getMarkerGroupUpRadius()) {
+                        isSeperate = false;
+                    }
+                }
+            }
+            if(isSeperate)
+                uniqueIndexes.put(i, List.of(currentCenterCoordinate));
+            else {
+                List<Vector2i> currentList = new ArrayList<>(uniqueIndexes.get(currentUniqueIndex).stream().toList());
+                currentList.add(currentCenterCoordinate);
+                uniqueIndexes.put(currentUniqueIndex, currentList);
             }
         }
+        for (Integer index : uniqueIndexes.keySet()) {
+            spawnMapMarkers[index].assignNewCenter(averagePosition(uniqueIndexes.get(index)));
+            spawnMapMarkers[index].draw(context);
+        }
+    }
+
+    private Vector2i averagePosition(List<Vector2i> listOfPositions){
+        Vector2i average = new Vector2i();
+        for(Vector2i position : listOfPositions){
+            average.x += position.x;
+            average.y += position.y;
+        }
+        average.x /= listOfPositions.size();
+        average.y /= listOfPositions.size();
+        return average;
     }
 }
--- src/main/java/net/jukoz/me/client/screens/utils/widgets/map/MapMarkerWidget.java
+++ src/main/java/net/jukoz/me/client/screens/utils/widgets/map/MapMarkerWidget.java
@@ -8,12 +8,18 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.joml.Vector2d;
+import org.joml.Vector2i;
 
 public class MapMarkerWidget extends ModWidget {
     private static final Identifier MAP_MARKERS = Identifier.of(MiddleEarth.MOD_ID,"textures/gui/widget/map_markers.png");
 
     ButtonWidget markerButton;
     MapMarkerType type;
+
+    // runtime
+    Vector2i runtimeStartCoordinates = null;
+    Vector2i runtimeUvs = null;
+    Vector2i runtimeSize = null;
 
     public MapMarkerWidget(String name, ButtonWidget.PressAction onPress) {
         super();
@@ -29,44 +35,66 @@ public class MapMarkerWidget extends ModWidget {
         return markerButton;
     }
 
-    public void drawFromWorldPosition(DrawContext context, MapWidget mapWidget, Vector2d worldPosition){
+    public void computeFromWorldPosition(MapWidget mapWidget, Vector2d worldPosition){
         Vector2d uvs = mapWidget.getMapPointFromWorldCoordinate(worldPosition);
-        drawCentered(context, mapWidget, uvs);
+        computeCentered(mapWidget, uvs);
     }
 
-    public void drawFromMapPosition(DrawContext context, MapWidget mapWidget, Vector2d mapPosition){
+    public void computeFromMapPosition(MapWidget mapWidget, Vector2d mapPosition){
         Vector2d uvs = mapWidget.getMapPointFromMapCoordinate(mapPosition);
-        drawCentered(context, mapWidget, uvs);
+        computeCentered(mapWidget, uvs);
     }
 
-    private void drawCentered(DrawContext context, MapWidget mapWidget, Vector2d centerUvs){
+    private void computeCentered(MapWidget mapWidget, Vector2d centerUvs){
         if(centerUvs == null) return;
         MapMarkerArrowDirection outOfBoundDirection = mapWidget.isOutsideBounds(centerUvs, type.sizeX /2, type.sizeY / 2);
 
         centerUvs.x -= type.sizeX / 2.0;
         centerUvs.y -= type.sizeY / 2.0;
         if(outOfBoundDirection == MapMarkerArrowDirection.NONE) {
-            draw(context, centerUvs);
+            computeMarker(centerUvs);
             return;
         }
-        drawArrow(context, mapWidget, outOfBoundDirection, centerUvs);
+        computeArrow(mapWidget, outOfBoundDirection, centerUvs);
     }
 
-    protected void drawArrow(DrawContext context, MapWidget mapWidget, MapMarkerArrowDirection direction, Vector2d starts) {
+    protected void computeArrow(MapWidget mapWidget, MapMarkerArrowDirection direction, Vector2d starts) {
+        int minX = mapWidget.startX;
+        int minY = mapWidget.startY;
+        int maxX = mapWidget.startX + mapWidget.uiWidth - direction.sizeX;
+        int maxY = mapWidget.startY + mapWidget.uiHeight - direction.sizeY;
+
+        runtimeStartCoordinates = new Vector2i(
+                Math.min(maxX, Math.max(minX, (int) starts.x)),
+                Math.min(maxY, Math.max(minY, (int) starts.y)));
+        runtimeUvs = new Vector2i(direction.uvX, direction.uvY);
+        runtimeSize = new Vector2i(direction.sizeX, direction.sizeY);
+    }
+
+    protected void computeMarker(Vector2d starts) {
+        runtimeStartCoordinates = new Vector2i((int) starts.x, (int) starts.y);
+        runtimeUvs = new Vector2i(type.uvX, type.uvY);
+        runtimeSize = new Vector2i(type.sizeX, type.sizeY);
+    }
+
+    public void draw(DrawContext context) {
         context.drawTexture(MAP_MARKERS,
-                Math.min(mapWidget.startX + mapWidget.uiWidth - direction.sizeX, Math.max(mapWidget.startX, (int) starts.x)),
-                Math.min(mapWidget.startY + mapWidget.uiHeight - direction.sizeY, Math.max(mapWidget.startY, (int) starts.y)),
-                direction.uvX, direction.uvY,
-                direction.sizeX, direction.sizeY
+                runtimeStartCoordinates.x,
+                runtimeStartCoordinates.y,
+                runtimeUvs.x, runtimeUvs.y,
+                runtimeSize.x, runtimeSize.y
         );
     }
 
-    protected void draw(DrawContext context, Vector2d starts) {
-        context.drawTexture(MAP_MARKERS,
-                (int) starts.x,
-                (int) starts.y,
-                type.uvX, type.uvY,
-                type.sizeX, type.sizeY
-        );
+    public Vector2i getCenterCoordinates() {
+        Vector2i startCoordinates = runtimeStartCoordinates;
+        startCoordinates.x += runtimeSize.x / 2;
+        startCoordinates.y += runtimeSize.y / 2;
+        return startCoordinates;
+    }
+
+    public void assignNewCenter(Vector2i newCenter) {
+        this.runtimeStartCoordinates.x = newCenter.x - (runtimeSize.x / 2);
+        this.runtimeStartCoordinates.y = newCenter.y - (runtimeSize.y / 2);
     }
 }
