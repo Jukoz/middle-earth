2a95d5d795df5e13e0f843c6a82bed8202c9c2f7
SlooshyBoi <robin.hugovv@gmail.com>
2024-10-15 01:19:00 +0200

    Remove sout

--- src/main/java/net/jukoz/me/MiddleEarthClient.java
+++ src/main/java/net/jukoz/me/MiddleEarthClient.java
@@ -652,7 +652,6 @@ public class MiddleEarthClient implements ClientModInitializer {
     }
 
     private void registerDyeableItem(Item item) {
-        System.out.println(item);
         Boolean overlay = ModDyeablePieces.dyeablePieces.get(item);
         if (overlay){
             ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : CustomDyeableDataComponent.getColor(stack, CustomDyeableDataComponent.DEFAULT_COLOR), item);
