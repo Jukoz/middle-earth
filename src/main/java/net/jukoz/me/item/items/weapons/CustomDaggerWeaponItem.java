e526ae995b9d0c02e91db178acba02a96e7a27ad
Number_Sir <number_sir@126.com>
2024-10-29 03:37:57 +0800

    fix: add missing "weapon_type" text prefix to tooltips for weapons

--- src/main/java/net/jukoz/me/item/items/weapons/CustomDaggerWeaponItem.java
+++ src/main/java/net/jukoz/me/item/items/weapons/CustomDaggerWeaponItem.java
@@ -43,14 +43,14 @@ public class CustomDaggerWeaponItem extends ReachWeaponItem {
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
         tooltip.add(Text.of(""));
         if (Screen.hasShiftDown()) {
-            if(this.type != null){
-                tooltip.add(Text.translatable("tooltip." + MiddleEarth.MOD_ID + "." + this.type.name));
-            }
             if(this.faction != null){
                 tooltip.add(Text.translatable("tooltip." + MiddleEarth.MOD_ID + ".faction").append(Text.translatable("tooltip." + MiddleEarth.MOD_ID + "." + faction.getName())));
             }
             if (this.subFaction != null) {
                 tooltip.add(Text.translatable("tooltip." + MiddleEarth.MOD_ID + ".sub_faction").append(Text.translatable("tooltip." + MiddleEarth.MOD_ID + "." + subFaction.getName())));
+            }
+            if(this.type != null){
+                tooltip.add(Text.translatable("tooltip." + MiddleEarth.MOD_ID + ".weapon_type").append(Text.translatable("tooltip." + MiddleEarth.MOD_ID + "." + this.type.name)));
             }
             tooltip.add(Text.translatable("tooltip." + MiddleEarth.MOD_ID + ".backstab"));
 
