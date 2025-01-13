f3cd55fc3bddb9b2a23b7019e5eb8f1b4cc59029
krahab <remygarenc@gmail.com>
2025-01-13 12:43:42 -0500

    Add new version

--- src/main/java/net/jukoz/me/MiddleEarth.java
+++ src/main/java/net/jukoz/me/MiddleEarth.java
@@ -35,9 +35,9 @@ import net.jukoz.me.world.dimension.ModDimensions;
 
 public class MiddleEarth implements ModInitializer {
 	public static final String MOD_ID = "me";
-	public static final String MOD_VERSION = "1.5.0-1.21.1-alpha";
+	public static final String MOD_VERSION = "1.5.1-1.21.1-alpha";
 	public static final boolean IS_DEBUG = true;
-	public static final boolean ENABLE_INSTANT_BOOTING = false;
+	public static final boolean ENABLE_INSTANT_BOOTING = true;
 
 	@Override
 	public void onInitialize() {
