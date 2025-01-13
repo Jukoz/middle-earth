25b97a5610d62307dbeccd48db927a49877538de
krahab <remygarenc@gmail.com>
2025-01-13 00:43:36 -0500

    Fix issue with layers

--- src/main/java/net/jukoz/me/block/special/LayersAltBlock.java
+++ src/main/java/net/jukoz/me/block/special/LayersAltBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 public class LayersAltBlock extends SnowBlock implements Waterloggable {
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
@@ -70,5 +71,10 @@ public class LayersAltBlock extends SnowBlock implements Waterloggable {
         } else {
             return i == 0;
         }
+    }
+
+    @Override
+    protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return true;
     }
 }
