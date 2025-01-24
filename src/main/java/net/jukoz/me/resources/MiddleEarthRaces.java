8e6db868004cd67e63780f23566c3847d0370aaa
Jukoz <olegptk12@gmail.com>
2025-01-24 16:14:11 -0500

    Made Uruk race +1 heart and -10% speed instead

--- src/main/java/net/jukoz/me/resources/MiddleEarthRaces.java
+++ src/main/java/net/jukoz/me/resources/MiddleEarthRaces.java
@@ -104,10 +104,10 @@ public class MiddleEarthRaces {
         URUK = new Race(Identifier.of(MiddleEarth.MOD_ID, "uruk"), RaceType.URUK,
                 new AttributeData(new HashMap<>(){{
                     put(EntityAttributes.GENERIC_SCALE, 1.0);
-                    put(EntityAttributes.GENERIC_MAX_HEALTH,18.0);
+                    put(EntityAttributes.GENERIC_MAX_HEALTH,22.0);
                     put(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1.0);
                     put(EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE, 3.0);
-                    put(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.11);
+                    put(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.09);
                     put(EntityAttributes.GENERIC_BURNING_TIME, 0.70);
                 }}), List.of(), List.of());
     }
