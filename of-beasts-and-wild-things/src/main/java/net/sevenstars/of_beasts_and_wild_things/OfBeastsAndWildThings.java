1e2fd32ceb66b3c1265a782a254c165db1963a2a
krahab <remygarenc@gmail.com>
2025-02-07 08:48:18 -0500

    Setup the mod

---
+++ of-beasts-and-wild-things/src/main/java/net/sevenstars/of_beasts_and_wild_things/OfBeastsAndWildThings.java
@@ -0,0 +1,17 @@
+package net.sevenstars.of_beasts_and_wild_things;
+
+import net.fabricmc.api.ModInitializer;
+import net.sevenstars.api.utils.ModLogger;
+
+public class OfBeastsAndWildThings implements ModInitializer {
+	public static final String MOD_ID = "wild-things";
+	public static final String MOD_VERSION = "1.5.3-1.21.4-alpha";
+	public static final boolean IS_DEBUG = true;
+	public static final ModLogger LOGGER = new ModLogger(MOD_ID, IS_DEBUG);
+	@Override
+	public void onInitialize() {
+
+		LOGGER.logInfoMsg("Testing the Of Beasts and Wild Things");
+
+	}
+}
---
+++ of-beasts-and-wild-things/src/main/java/net/sevenstars/of_beasts_and_wild_things/OfBeastsAndWildThingsClient.java
@@ -0,0 +1,13 @@
+package net.sevenstars.of_beasts_and_wild_things;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OfBeastsAndWildThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+        OfBeastsAndWildThings.LOGGER.logInfoMsg("Testing the Of Beasts and Wild Things - CLIENT");
+
+    }
+
+}
