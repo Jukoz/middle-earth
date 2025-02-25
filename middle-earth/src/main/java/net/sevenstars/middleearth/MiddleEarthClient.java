e582a1c84fd854e6d883ddd5c8d68b0263f681a8
SlooshyBoi <robin.hugovv@gmail.com>
2025-02-25 01:49:11 +0100

    Fix block loot table datagen

--- middle-earth/src/main/java/net/sevenstars/middleearth/MiddleEarthClient.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/MiddleEarthClient.java
@@ -113,16 +113,6 @@ public class MiddleEarthClient implements ClientModInitializer {
         EntityRendererRegistry.register(ModEntities.SPEAR, SpearEntityRenderer::new);
         EntityRendererRegistry.register(ModEntities.BOULDER, BoulderEntityRenderer::new);
 
-        //TODO fix this
-        /*SimpleDyeableItemModel.items.forEach(this::registerDyeableItem);
-
-        registerDyeableItem(ModEquipmentItems.BROADHOOF_GOAT_PADDED_ARMOR);
-        registerDyeableItem(ModEquipmentItems.BROADHOOF_GOAT_ORNAMENTED_PADDED_ARMOR);
-
-        registerDyeableItem(ModEquipmentItems.WARG_LEATHER_ARMOR);
-        registerDyeableItem(ModEquipmentItems.WARG_REINFORCED_LEATHER_ARMOR);*/
-
-
         // Animals
         EntityRendererRegistry.register(ModEntities.SWAN, SwanRenderer::new);
         EntityRendererRegistry.register(ModEntities.PHEASANT, PheasantRenderer::new);
@@ -701,8 +691,4 @@ public class MiddleEarthClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(ModNatureBlocks.GLOWWORM_MAIN, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModNatureBlocks.GLOWWORM_WEBBING, RenderLayer.getCutout());
     }
-
-    /*private void registerDyeableItem(Item item) {
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex > 0 ? -1 : CustomDyeableDataComponent.getColor(stack, CustomDyeableDataComponent.DEFAULT_COLOR), item);
-    }*/
 }
--- middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/providers/BlockLootTableProvider.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/providers/BlockLootTableProvider.java
@@ -2,8 +2,45 @@ package net.sevenstars.middleearth.datageneration.providers;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.condition.MatchToolLootCondition;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.TableBonusLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.predicate.NumberRange;
+import net.minecraft.predicate.StatePredicate;
+import net.minecraft.predicate.item.EnchantmentPredicate;
+import net.minecraft.predicate.item.EnchantmentsPredicate;
+import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.predicate.item.ItemSubPredicateTypes;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.sevenstars.middleearth.block.*;
+import net.sevenstars.middleearth.block.special.LargeDoorBlock;
+import net.sevenstars.middleearth.block.special.RocksBlock;
+import net.sevenstars.middleearth.block.special.verticalSlabs.VerticalSlabBlock;
+import net.sevenstars.middleearth.datageneration.content.loot_tables.BlockDrops;
+import net.sevenstars.middleearth.datageneration.content.loot_tables.CropDrops;
+import net.sevenstars.middleearth.datageneration.content.loot_tables.LeavesDrops;
+import net.sevenstars.middleearth.datageneration.content.loot_tables.PotDrops;
+import net.sevenstars.middleearth.datageneration.content.models.SimplePaneModel;
+import net.sevenstars.middleearth.datageneration.content.models.SimpleRocksModel;
+import net.sevenstars.middleearth.datageneration.content.models.TintableCrossModel;
+import net.sevenstars.middleearth.item.ModResourceItems;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class BlockLootTableProvider extends FabricBlockLootTableProvider {
@@ -15,12 +52,6 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
         this.registryLookup = registryLookup;
     }
 
-    @Override
-    public void generate() {
-
-    }
-
-    /*
     @Override
     public void generate() {
 
@@ -200,7 +231,7 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
     public LootTable.Builder verticalSlabDrops(Block drop) {
         return LootTable.builder().pool(
                 LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
-                        .with((LootPoolEntry.Builder)this.applyExplosionDecay(drop, ItemEntry.builder(drop).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(2.0F)).conditionally(BlockStatePropertyLootCondition.builder(drop).properties(StatePredicate.Builder.create().exactMatch(VerticalSlabBlock.DOUBLE, true)))))));
+                        .with(this.applyExplosionDecay(drop, ItemEntry.builder(drop).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(2.0F)).conditionally(BlockStatePropertyLootCondition.builder(drop).properties(StatePredicate.Builder.create().exactMatch(VerticalSlabBlock.DOUBLE, true)))))));
     }
 
     public void cobbleDrops(Block stoneBlock, Block cobbledBlock) {
@@ -224,6 +255,6 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
     }
 
     public void largeDoorDrop(Block doorblock) {
-        addDrop(doorblock, LootTable.builder().pool((LootPool.Builder) this.addSurvivesExplosionCondition(doorblock, LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F)).with(ItemEntry.builder(doorblock).conditionally(BlockStatePropertyLootCondition.builder(doorblock).properties(StatePredicate.Builder.create().exactMatch(LargeDoorBlock.PART, 0)))))));
-    }*/
+        addDrop(doorblock, LootTable.builder().pool(this.addSurvivesExplosionCondition(doorblock, LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F)).with(ItemEntry.builder(doorblock).conditionally(BlockStatePropertyLootCondition.builder(doorblock).properties(StatePredicate.Builder.create().exactMatch(LargeDoorBlock.PART, 0)))))));
+    }
 }
