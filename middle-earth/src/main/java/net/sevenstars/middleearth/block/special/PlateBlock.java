80f3f638a2931267989ea33ff52bb8e811200d31
Jukoz <olegptk12@gmail.com>
2025-04-28 13:23:54 -0400

    Add outline shape plate

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/PlateBlock.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/PlateBlock.java
@@ -1,9 +1,15 @@
 package net.sevenstars.middleearth.block.special;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class PlateBlock extends HorizontalFacingBlock {
 
@@ -15,6 +21,11 @@ public class PlateBlock extends HorizontalFacingBlock {
         this.setDefaultState(((this.stateManager.getDefaultState())
                 .with(UTENSILS, false))
                 .with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return Block.createCuboidShape(3.0, 0.0, 3.0, 13.0, 1.0, 13.0);
     }
 
     @Override
