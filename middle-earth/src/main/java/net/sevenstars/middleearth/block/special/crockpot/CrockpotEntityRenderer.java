d189fe648dbd1ace5f18035b61d3ccd8e5e9bb25
Jukoz <olegptk12@gmail.com>
2025-04-30 23:30:34 -0400

    Fix UV coordinates strecthing and cleaning code

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/crockpot/CrockpotEntityRenderer.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/crockpot/CrockpotEntityRenderer.java
@@ -15,12 +15,14 @@ import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import org.lwjgl.system.MathUtil;
 
 @Environment(value= EnvType.CLIENT)
 public class CrockpotEntityRenderer implements BlockEntityRenderer<CrockpotBlockEntity> {
     private static final Identifier waterTexture = Identifier.ofVanilla("textures/block/water_still.png");
-    private static SpriteIdentifier waterSpriteId = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/water_flow"));
+    private static SpriteIdentifier waterSpriteId = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/water_still"));
 
     public CrockpotEntityRenderer(BlockEntityRendererFactory.Context context) {
     }
@@ -34,12 +36,9 @@ public class CrockpotEntityRenderer implements BlockEntityRenderer<CrockpotBlock
         int blue = color & 255;
         int alpha = 200;
 
-        float epsilon = 0.001f;
-
         matrices.push();
         matrices.translate(0, 0.3f, 0);
 
-        //vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(waterTexture, true));
         VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getTranslucentMovingBlock());
         matrices.peek();
 
@@ -48,30 +47,23 @@ public class CrockpotEntityRenderer implements BlockEntityRenderer<CrockpotBlock
         Sprite waterSprite = waterSpriteId.getSprite();
 
         MatrixStack.Entry entry = matrices.peek();
-        //consumer.quad(entry, consumer, color);
-        //consumer.vertex(entry, 0, 0.3f, 0).color(color).texture(5, 5).overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(entry, 0.0F, 1.0F, 0.0F);
 
-        // Morgul Alchemy
-        //consumer.vertex(entry, sizeFactor, 0, 1 - sizeFactor).color(255, 50, 80, 190).texture(0, 0).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, 1 - sizeFactor).color(255, 50, 80, 190).texture(0, 1).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, sizeFactor).color(255, 50, 80, 190).texture(1, 0).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, sizeFactor, 0, sizeFactor).color(255, 50, 80, 190).texture(1, 1).light(light).overlay(overlay).normal(1, 1, 1);
+        float minU = waterSprite.getMinU();
+        float minV = waterSprite.getMinV();
+        float maxU = waterSprite.getMaxU();
+        float maxV = waterSprite.getMaxV();
 
-        // Horizontal
-        //consumer.vertex(entry, sizeFactor, 0, 1 - sizeFactor).color(50, 80, 240, 190).texture(0, 1).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, 1 - sizeFactor).color(50, 80, 190, 190).texture(1, 1).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, sizeFactor).color(50, 80, 240, 190).texture(1, 0).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, sizeFactor, 0, sizeFactor).color(50, 80, 240, 190).texture(0, 0).light(light).overlay(overlay).normal(1, 1, 1);
+        float tempMin = minU;
+        minU = MathHelper.lerp(sizeFactor, minU, maxU);
+        maxU = MathHelper.lerp(1 - sizeFactor, tempMin, maxU);
+        tempMin = minV;
+        minV = MathHelper.lerp(sizeFactor, minV, maxV);
+        maxV = MathHelper.lerp(1 - sizeFactor, tempMin, maxV);
 
-        //consumer.vertex(entry, sizeFactor, 0, 1 - sizeFactor).color(50, 80, 240, 190).texture(1, 1).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, 1 - sizeFactor).color(50, 80, 190, 190).texture(0, 1).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, 1 - sizeFactor, 0, sizeFactor).color(50, 80, 240, 190).texture(0, 0).light(light).overlay(overlay).normal(1, 1, 1);
-        //consumer.vertex(entry, sizeFactor, 0, sizeFactor).color(50, 80, 240, 190).texture(1, 0).light(light).overlay(overlay).normal(1, 1, 1);
-
-        consumer.vertex(entry, sizeFactor, 0, sizeFactor)               .color(50, 80, 240, 190).texture(waterSprite.getMinU(), waterSprite.getMinV()).light(light).overlay(overlay).normal(1, 1, 1);
-        consumer.vertex(entry, sizeFactor, 0, 1 - sizeFactor)  .color(50, 80, 190, 190).texture(waterSprite.getMinU(), waterSprite.getMaxV()).light(light).overlay(overlay).normal(1, 1, 1);
-        consumer.vertex(entry, 1 - sizeFactor, 0, 1 - sizeFactor)        .color(50, 80, 240, 190).texture(waterSprite.getMaxU(), waterSprite.getMaxV()).light(light).overlay(overlay).normal(1, 1, 1);
-        consumer.vertex(entry, 1 - sizeFactor, 0, sizeFactor)               .color(50, 80, 240, 190).texture(waterSprite.getMaxU(), waterSprite.getMinV()).light(light).overlay(overlay).normal(1, 1, 1);
+        consumer.vertex(entry, sizeFactor, 0, sizeFactor)               .color(50, 80, 240, 190).texture(minU, minV).light(light).overlay(overlay).normal(1, 1, 1);
+        consumer.vertex(entry, sizeFactor, 0, 1 - sizeFactor)  .color(50, 80, 190, 190).texture(minU, maxV).light(light).overlay(overlay).normal(1, 1, 1);
+        consumer.vertex(entry, 1 - sizeFactor, 0, 1 - sizeFactor)        .color(50, 80, 240, 190).texture(maxU, maxV).light(light).overlay(overlay).normal(1, 1, 1);
+        consumer.vertex(entry, 1 - sizeFactor, 0, sizeFactor)               .color(50, 80, 240, 190).texture(maxU, minV).light(light).overlay(overlay).normal(1, 1, 1);
 
         matrices.pop();
     }
