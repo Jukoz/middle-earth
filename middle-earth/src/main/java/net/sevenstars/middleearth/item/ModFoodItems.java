dae0e2e3723d150d15c5faaf0ddbb7514a9a7aea
Jukoz <olegptk12@gmail.com>
2025-05-15 11:14:08 -0400

    Fix skewer items remainder

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/ModFoodItems.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/ModFoodItems.java
@@ -110,19 +110,19 @@ public class ModFoodItems {
                     new FoodComponent.Builder().nutrition(9).saturationModifier(0.9f).build()).maxCount(1).useRemainder(Items.BOWL));
     public static final Item MEAT_SKEWER = registerItem("meat_skewer",
             Item::new,new Item.Settings().food(
-                    new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build()).maxCount(8).useRemainder(Items.BOWL));
+                    new FoodComponent.Builder().nutrition(5).saturationModifier(0.6f).build()).maxCount(8).useRemainder(Items.STICK));
     public static final Item COOKED_MEAT_SKEWER = registerItem("cooked_meat_skewer",
             Item::new,new Item.Settings().food(
-                    new FoodComponent.Builder().nutrition(10).saturationModifier(0.8f).build()).maxCount(8).useRemainder(Items.BOWL));
+                    new FoodComponent.Builder().nutrition(10).saturationModifier(0.8f).build()).maxCount(8).useRemainder(Items.STICK));
     public static final Item POULTRY_MEAL = registerItem("poultry_meal",
             Item::new,new Item.Settings().food(
                     new FoodComponent.Builder().nutrition(8).saturationModifier(0.8f).build()).maxCount(8).useRemainder(Items.BOWL));
     public static final Item VEGETABLE_SKEWER = registerItem("vegetable_skewer",
             Item::new,new Item.Settings().food(
-                    new FoodComponent.Builder().nutrition(3).saturationModifier(0.3f).build()).maxCount(8).useRemainder(Items.BOWL));
+                    new FoodComponent.Builder().nutrition(3).saturationModifier(0.3f).build()).maxCount(8).useRemainder(Items.STICK));
     public static final Item COOKED_VEGETABLE_SKEWER = registerItem("cooked_vegetable_skewer",
             Item::new,new Item.Settings().food(
-                    new FoodComponent.Builder().nutrition(6).saturationModifier(0.6f).build()).maxCount(8).useRemainder(Items.BOWL));
+                    new FoodComponent.Builder().nutrition(6).saturationModifier(0.6f).build()).maxCount(8).useRemainder(Items.STICK));
     public static final Item VEGETABLE_SOUP = registerItem("vegetable_soup",
             Item::new,new Item.Settings().food(
                     new FoodComponent.Builder().nutrition(7).saturationModifier(0.6f).build()).maxCount(1).useRemainder(Items.BOWL));
