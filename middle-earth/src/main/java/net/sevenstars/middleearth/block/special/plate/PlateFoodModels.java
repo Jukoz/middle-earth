d76e47030b11ae843b61096f04046e4a4b870283
Jukoz <67010061+Jukoz@users.noreply.github.com>
2025-05-25 18:37:58 -0400

    Add berry pie

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/plate/PlateFoodModels.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/plate/PlateFoodModels.java
@@ -29,6 +29,7 @@ public class PlateFoodModels {
         addVanillaModel("pumpkin_pie");
         addVanillaModel("rabbit_stew");
         addModel("cram");
+        addModel("berry_pie");
         addModel("meat_skewer");
         addModel("cooked_meat_skewer");
         addModel("vegetable_skewer");
