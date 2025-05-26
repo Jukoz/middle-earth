00481943ddfdaf3c15677c9cd9c1bce57daff0e9
Jukoz <67010061+Jukoz@users.noreply.github.com>
2025-05-26 10:03:00 -0400

    Slicing the cake

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/LayeredCakeBlock.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/LayeredCakeBlock.java
@@ -6,7 +6,11 @@ import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -19,14 +23,15 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.tick.ScheduledTickView;
 
 public class LayeredCakeBlock extends Block {
     public static final MapCodec<net.minecraft.block.CakeBlock> CODEC = createCodec(net.minecraft.block.CakeBlock::new);
-    //public static final int MAX_BITES = 14;
-    //public static final IntProperty BITES;
+    public static final int MAX_BITES = 8;
+    public static final IntProperty BITES;
     public static final int DEFAULT_COMPARATOR_OUTPUT;
-    //private static final VoxelShape[] SHAPES_BY_BITES;
+    private static final VoxelShape[] SHAPES_BY_BITES;
     private static final VoxelShape SHAPE;
 
     public MapCodec<net.minecraft.block.CakeBlock> getCodec() {
@@ -39,7 +44,7 @@ public class LayeredCakeBlock extends Block {
     }
 
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        return SHAPES_BY_BITES[state.get(BITES)];
     }
 
     protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
@@ -78,16 +83,16 @@ public class LayeredCakeBlock extends Block {
         if (!player.canConsume(false)) {
             return ActionResult.PASS;
         } else {
-            /* player.incrementStat(Stats.EAT_CAKE_SLICE);
-            player.getHungerManager().add(2, 0.1F);
-            int i = (Integer)state.get(BITES);
+            player.incrementStat(Stats.EAT_CAKE_SLICE);
+            player.getHungerManager().add(3, 0.15F);
+            int i = state.get(BITES);
             world.emitGameEvent(player, GameEvent.EAT, pos);
-            if (i < 6) {
-                world.setBlockState(pos, (BlockState)state.with(BITES, i + 1), 3);
+            if (i < MAX_BITES) {
+                world.setBlockState(pos, state.with(BITES, i + 1), 3);
             } else {
                 world.removeBlock(pos, false);
                 world.emitGameEvent(player, GameEvent.BLOCK_DESTROY, pos);
-            } */
+            }
 
             return ActionResult.SUCCESS;
         }
@@ -102,15 +107,15 @@ public class LayeredCakeBlock extends Block {
     }
 
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        //builder.add(new Property[]{BITES});
+        builder.add(BITES);
     }
 
     protected int getComparatorOutput(BlockState state, World world, BlockPos pos) {
-        return 0;// getComparatorOutput((Integer)state.get(BITES));
+        return getComparatorOutput(state.get(BITES));
     }
 
     public static int getComparatorOutput(int bites) {
-        return (14 - bites) * 2;
+        return (MAX_BITES - bites) * 2;
     }
 
     protected boolean hasComparatorOutput(BlockState state) {
@@ -122,12 +127,13 @@ public class LayeredCakeBlock extends Block {
     }
 
     static {
-        //BITES = Properties.BITES;
+        BITES = IntProperty.of("bites", 0, MAX_BITES);;
         DEFAULT_COMPARATOR_OUTPUT = getComparatorOutput(0);
         SHAPE = VoxelShapes.union(Block.createCuboidShape(0, 0.0, 16.0, 16.0, 8.0, 16.0),
             Block.createCuboidShape(2, 8, 2, 14, 16, 14));
-        //SHAPES_BY_BITES = Block.createShapeArray(6, (bites) -> {
-        //    return Block.createCuboidShape((double)(1 + bites * 2), 0.0, 1.0, 15.0, 8.0, 15.0);
-        //});
+        SHAPES_BY_BITES = Block.createShapeArray(MAX_BITES, (bites) -> {
+            return VoxelShapes.union(Block.createCuboidShape(0, 0.0, 0, 16.0, 8.0, 16.0),
+                    Block.createCuboidShape(2, 8, 2 + (Math.clamp(bites, 0, 4) * 3), 14, 16, 14));
+        });
     }
 }
