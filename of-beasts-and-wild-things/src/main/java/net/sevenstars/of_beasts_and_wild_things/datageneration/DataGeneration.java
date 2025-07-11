14e06f6059e57fc4fa56783e0da58dea9eee4f5c
SlooshyBoi <robin.hugovv@gmail.com>
2025-07-11 18:20:01 +0200

    Add swan prey tag datagen

--- of-beasts-and-wild-things/src/main/java/net/sevenstars/of_beasts_and_wild_things/datageneration/DataGeneration.java
+++ of-beasts-and-wild-things/src/main/java/net/sevenstars/of_beasts_and_wild_things/datageneration/DataGeneration.java
@@ -2,6 +2,7 @@ package net.sevenstars.of_beasts_and_wild_things.datageneration;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.sevenstars.of_beasts_and_wild_things.datageneration.providers.EntityTagProvider;
 import net.sevenstars.of_beasts_and_wild_things.datageneration.providers.LanguageProvider;
 import net.sevenstars.of_beasts_and_wild_things.datageneration.providers.ModelProvider;
 
@@ -16,5 +17,6 @@ public class DataGeneration implements DataGeneratorEntrypoint {
 
         pack.addProvider(ModelProvider::new);
         pack.addProvider(LanguageProvider::new);
+        pack.addProvider(EntityTagProvider::new);
     }
 }
