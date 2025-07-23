ef908dd46256df36b070de6208b83fe0e50e51dc
Jukoz <olegptk12@gmail.com>
2025-07-23 15:48:54 -0400

    Add recipe output result from first match

--- middle-earth/src/main/java/net/sevenstars/middleearth/gui/inscriptiontable/InscriptionTableScreenHandler.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/gui/inscriptiontable/InscriptionTableScreenHandler.java
@@ -1,5 +1,8 @@
 package net.sevenstars.middleearth.gui.inscriptiontable;
 
+import com.mojang.authlib.GameProfile;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftingResultInventory;
@@ -8,29 +11,36 @@ import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeEntry;
+import net.minecraft.recipe.ServerRecipeManager;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.CraftingResultSlot;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.sevenstars.middleearth.block.ModDecorativeBlocks;
+import net.sevenstars.middleearth.block.special.forge.MultipleStackRecipeInput;
 import net.sevenstars.middleearth.gui.ModScreenHandlers;
 import net.sevenstars.middleearth.gui.artisantable.ArtisanTableInputsShape;
 import net.sevenstars.middleearth.gui.artisantable.ArtisanTableScreenHandler;
 import net.sevenstars.middleearth.gui.artisantable.ArtisanTableSlot;
-import net.sevenstars.middleearth.recipe.ArtisanRecipe;
+import net.sevenstars.middleearth.recipe.InscriptionRecipe;
+import net.sevenstars.middleearth.recipe.ModRecipes;
+import net.sevenstars.middleearth.resources.datas.Disposition;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class InscriptionTableScreenHandler extends ScreenHandler {
     private final ScreenHandlerContext context;
     private final World world;
-    private List<RecipeEntry<ArtisanRecipe>> availableRecipes;
+    private RecipeEntry<InscriptionRecipe> outputRecipe;
     private ItemStack inputStack;
     long lastTakeTime;
     private ArtisanTableSlot[][] inputSlots;
@@ -50,7 +60,13 @@ public class InscriptionTableScreenHandler extends ScreenHandler {
         super(ModScreenHandlers.ARTISAN_SCREEN_HANDLER, syncId);
         this.inputStack = ItemStack.EMPTY;
 
-        this.input = new SimpleInventory(8);
+        this.input = new SimpleInventory(7) {
+            public void markDirty() {
+                super.markDirty();
+                InscriptionTableScreenHandler.this.updateInput(this);
+            }
+        };
+        this.output = new CraftingResultInventory();
 
         this.addSlot(new Slot(this.input, 0, 47, 35));
         this.addSlot(new Slot(this.input, 1, 47, 15));
@@ -60,7 +76,7 @@ public class InscriptionTableScreenHandler extends ScreenHandler {
         this.addSlot(new Slot(this.input, 5, 27, 45));
         this.addSlot(new Slot(this.input, 6, 27, 25));
 
-        this.outputSlot = this.addSlot(new Slot(this.input, 7, 129, 35) {
+        this.outputSlot = this.addSlot(new Slot(this.output, 7, 129, 35) {
             @Override
             public boolean canInsert(ItemStack stack) {
                 return false;
@@ -92,7 +108,6 @@ public class InscriptionTableScreenHandler extends ScreenHandler {
             }
         });
 
-        this.output = new CraftingResultInventory();
         this.context = context;
         this.world = playerInventory.player.getWorld();
 
@@ -104,6 +119,45 @@ public class InscriptionTableScreenHandler extends ScreenHandler {
         this.playerEntity = player;
         return canUse(this.context, player, ModDecorativeBlocks.INSCRIPTION_TABLE);
     }
+
+
+    private void updateInput(Inventory inventory) {
+        List<ItemStack> inputs = new ArrayList<>();
+        for (int i = 0; i < inventory.size(); i++) {
+            inputs.add(inventory.getStack(i));
+        }
+        this.outputSlot.setStackNoCallbacks(ItemStack.EMPTY);
+        if (!inputs.isEmpty()) {
+            if (inputs.size() >= 2 && !this.world.isClient){
+                ServerRecipeManager serverRecipeManager = (ServerRecipeManager) this.world.getRecipeManager();
+                Optional<RecipeEntry<InscriptionRecipe>> optionalOutput = serverRecipeManager.getFirstMatch(ModRecipes.INSCRIPTION_TABLE, new MultipleStackRecipeInput(inputs), this.world);
+                optionalOutput.ifPresent(inscriptionRecipeRecipeEntry -> this.outputRecipe = inscriptionRecipeRecipeEntry);
+                populateResult();
+            }
+        }
+    }
+
+    void populateResult() {
+        if (this.outputRecipe != null && this.outputRecipe.value() != null) {
+            List<ItemStack> inputs = new ArrayList<>();
+            for (int i = 0; i < this.input.size(); i++) {
+                inputs.add(this.input.getStack(i));
+            }
+
+            ItemStack itemStack = outputRecipe.value().craft(new MultipleStackRecipeInput(inputs), this.world.getRegistryManager());
+
+            if (itemStack.isItemEnabled(this.world.getEnabledFeatures())) {
+                this.output.setLastRecipe(outputRecipe);
+                this.outputSlot.setStackNoCallbacks(itemStack);
+            } else {
+                this.outputSlot.setStackNoCallbacks(ItemStack.EMPTY);
+            }
+        } else {
+            this.outputSlot.setStackNoCallbacks(ItemStack.EMPTY);
+        }
+        this.sendContentUpdates();
+    }
+
 
     public ScreenHandlerType<?> getType() {
         return ModScreenHandlers.INSCRIPTION_SCREEN_HANDLER;
