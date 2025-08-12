be5941bfd61cb5a879c7ed68bd26f14def2e8621
Jukoz <olegptk12@gmail.com>
2025-08-12 14:39:14 -0400

    Add Mordor variant & tweak spawn conditions

--- middle-earth/src/main/java/net/sevenstars/middleearth/entity/spider/SpiderVariants.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/entity/spider/SpiderVariants.java
@@ -21,27 +21,29 @@ public class SpiderVariants {
 
 	public static final RegistryKey<SpiderVariant> MIRKWOOD = of("mirkwood");
 	public static final RegistryKey<SpiderVariant> CAVE = of("cave");
+	public static final RegistryKey<SpiderVariant> MORDOR = of("mordor");
 	public static final RegistryKey<SpiderVariant> DEFAULT = MIRKWOOD;
 
 	private static RegistryKey<SpiderVariant> of(String id) {
 		return RegistryKey.of(SpiderVariants.KEY, Identifier.of(MiddleEarth.MOD_ID, id));
 	}
 
-	private static void register(Registerable<SpiderVariant> registry, RegistryKey<SpiderVariant> key, String textureName, TagKey<Biome> biomeTag) {
-		register(registry, key, textureName, createSpawnConditions(registry.getRegistryLookup(RegistryKeys.BIOME).getOrThrow(biomeTag)));
+	private static void register(Registerable<SpiderVariant> registry, RegistryKey<SpiderVariant> key, String textureName, TagKey<Biome> biomeTag, int priority) {
+		register(registry, key, textureName, createSpawnConditions(registry.getRegistryLookup(RegistryKeys.BIOME).getOrThrow(biomeTag), priotity));
 	}
 
 	private static void register(Registerable<SpiderVariant> registry, RegistryKey<SpiderVariant> key, String textureName, SpawnConditionSelectors spawnConditions) {
 		registry.register(key, new SpiderVariant(new AssetInfo(Identifier.of(MiddleEarth.MOD_ID, TEXTURE_PATH + textureName)), spawnConditions));
 	}
 
-	private static SpawnConditionSelectors createSpawnConditions(RegistryEntryList<Biome> requiredBiomes) {
-		return SpawnConditionSelectors.createSingle(new BiomeSpawnCondition(requiredBiomes), 1);
+	private static SpawnConditionSelectors createSpawnConditions(RegistryEntryList<Biome> requiredBiomes, int priority) {
+		return SpawnConditionSelectors.createSingle(new BiomeSpawnCondition(requiredBiomes), priority);
 	}
 
 	public static void bootstrap(Registerable<SpiderVariant> registry) {
 		register(registry, MIRKWOOD, "mirkwood_shelobite_scuttler", SpawnConditionSelectors.createFallback(0));
-		register(registry, CAVE, "blind_shelobite_scuttler", TagKey.of(RegistryKeys.BIOME, Identifier.of(MiddleEarth.MOD_ID, "is_cave")));
+		register(registry, CAVE, "blind_shelobite_scuttler", TagKey.of(RegistryKeys.BIOME, Identifier.of(MiddleEarth.MOD_ID, "is_cave")), 1);
+		register(registry, MORDOR, "mordor_shelobite_scuttler", TagKey.of(RegistryKeys.BIOME, Identifier.of(MiddleEarth.MOD_ID, "is_mordor")), 2);
 	}
 
 	public static void register(){
