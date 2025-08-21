d96db4d9ce83ba394f3cb97ae8203713022fbacd
SlooshyBoi <robin.hugovv@gmail.com>
2025-08-21 06:18:13 +0200

    Start chalk set + add calcite window

--- middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/providers/models/BlockModelProvider.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/providers/models/BlockModelProvider.java
@@ -957,7 +957,7 @@ public class BlockModelProvider extends FabricModelProvider {
                                                            String modId, String topTexturePath, String bottomTexturePath, String sideTexturePath) {
         Identifier fullBlockId = ModelIds.getBlockModelId(origin);
         String modIdTopBottom = modId;
-        if (sideTexturePath.contains("deepslate_carved_window")){
+        if (sideTexturePath.contains("deepslate_carved_window") || sideTexturePath.contains("calcite_carved_window")){
             modIdTopBottom = "minecraft";
         }
 
