e7a0a8b1050cc2b60e3400f2a52f5ca258cd104d
Jukoz <olegptk12@gmail.com>
2025-10-27 13:32:41 -0400

    Add crown transition particles & tweaks

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/WoodlandRealmCrownItem.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/WoodlandRealmCrownItem.java
@@ -2,10 +2,14 @@ package net.sevenstars.middleearth.item.items.armor;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.particle.*;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.biome.Biome;
 import net.sevenstars.middleearth.item.DataComponentTypesME;
 import net.sevenstars.middleearth.item.dataComponents.SeasonDataComponent;
@@ -14,6 +18,9 @@ import net.sevenstars.middleearth.world.biomes.BiomeTagsME;
 import org.jetbrains.annotations.Nullable;
 
 public class WoodlandRealmCrownItem extends CustomHelmetItem {
+    private static final float OFFSET_XZ = 0.5f;
+    private static final float OFFSET_Y = -0.3f;
+    private SeasonDataComponent.Season season = SeasonDataComponent.Season.DEAD;
 
     public WoodlandRealmCrownItem(ExtendedArmorMaterial material, Settings settings) {
         super(material, settings.armor(material.material(), EquipmentType.HELMET).maxCount(1));
@@ -24,19 +31,41 @@ public class WoodlandRealmCrownItem extends CustomHelmetItem {
         super.inventoryTick(stack, world, entity, slot);
         RegistryEntry<Biome> biomeEntry = world.getBiome(entity.getBlockPos());
         if(biomeEntry.getKey().isPresent()) {
-            SeasonDataComponent seasonDataComponent = new SeasonDataComponent(SeasonDataComponent.Season.SUMMER);
 
+            SeasonDataComponent.Season newSeason = SeasonDataComponent.Season.SUMMER;
             if(biomeEntry.isIn(BiomeTagsME.SPRING)) {
-                seasonDataComponent = new SeasonDataComponent(SeasonDataComponent.Season.SPRING);
+                newSeason = SeasonDataComponent.Season.SPRING;
             } else if(biomeEntry.isIn(BiomeTagsME.AUTUMN)) {
-                seasonDataComponent = new SeasonDataComponent(SeasonDataComponent.Season.AUTUMN);
+                newSeason = SeasonDataComponent.Season.AUTUMN;
             } else if(biomeEntry.isIn(BiomeTagsME.WINTER)) {
-                seasonDataComponent = new SeasonDataComponent(SeasonDataComponent.Season.WINTER);
+                newSeason = SeasonDataComponent.Season.WINTER;
             } else if(biomeEntry.isIn(BiomeTagsME.DEAD)) {
-                seasonDataComponent = new SeasonDataComponent(SeasonDataComponent.Season.DEAD);
+                newSeason = SeasonDataComponent.Season.DEAD;
             }
 
-            stack.set(DataComponentTypesME.SEASON_DATA, seasonDataComponent);
+            if(season != newSeason) {
+                LivingEntity livingEntity = (LivingEntity) entity;
+                double scale = 1.8f * livingEntity.getAttributeValue(EntityAttributes.SCALE);
+                Vec3d pos = entity.getPos().add(0, 1.8f * scale, 0).add(0, -1.1f, 0);
+
+                if(season.equals(SeasonDataComponent.Season.DEAD) && !newSeason.equals(SeasonDataComponent.Season.WINTER)) {
+                    world.spawnParticles(ParticleTypes.COMPOSTER, pos.getX(), pos.getY(), pos.getZ(), 9, OFFSET_XZ, OFFSET_Y, OFFSET_XZ, 1);
+                } else if(season.equals(SeasonDataComponent.Season.SPRING)) {
+                    world.spawnParticles(ParticleTypes.CHERRY_LEAVES, pos.getX(), pos.getY(), pos.getZ(), 9, OFFSET_XZ, OFFSET_Y, OFFSET_XZ, 1);
+                } else if(season.equals(SeasonDataComponent.Season.SUMMER)) {
+                    int leavesColor = world.getBiome(entity.getBlockPos()).value().getFoliageColor();
+                    TintedParticleEffect tintedParticleEffect = TintedParticleEffect.create(ParticleTypes.TINTED_LEAVES, leavesColor);
+                    world.spawnParticles((ParticleEffect)tintedParticleEffect, pos.getX(), pos.getY(), pos.getZ(), 9, OFFSET_XZ, OFFSET_Y, OFFSET_XZ, 1);
+                } else if(season.equals(SeasonDataComponent.Season.AUTUMN)) {
+                    TintedParticleEffect tintedParticleEffect = TintedParticleEffect.create(ParticleTypes.TINTED_LEAVES, 8930366);
+                    world.spawnParticles((ParticleEffect)tintedParticleEffect, pos.getX(), pos.getY(), pos.getZ(), 9, OFFSET_XZ, OFFSET_Y, OFFSET_XZ, 1);
+                } else if(season.equals(SeasonDataComponent.Season.WINTER)) {
+                    world.spawnParticles(ParticleTypes.SNOWFLAKE, pos.getX(), pos.getY(), pos.getZ(), 12, OFFSET_XZ, OFFSET_Y, OFFSET_XZ, 0.05f);
+                }
+            }
+
+            season = newSeason;
+            stack.set(DataComponentTypesME.SEASON_DATA, new SeasonDataComponent(newSeason));
         }
     }
 }
