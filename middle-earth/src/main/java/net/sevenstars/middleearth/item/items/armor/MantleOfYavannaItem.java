74064e5bae1bc2df09b10a926fcef85a6eb31134
Jukoz <olegptk12@gmail.com>
2025-11-30 17:24:39 -0500

    Fix Yavanna mantle as back attachment

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/MantleOfYavannaItem.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/MantleOfYavannaItem.java
@@ -10,6 +10,8 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
+import net.sevenstars.middleearth.item.DataComponentTypesME;
+import net.sevenstars.middleearth.item.dataComponents.BackAttachmentDataComponent;
 import net.sevenstars.middleearth.item.utils.armor.ExtendedArmorMaterial;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,5 +47,28 @@ public class MantleOfYavannaItem extends BackAttachmentItem {
             currentColor = lerpColor.getRGB();
             stack.set(DataComponentTypes.DYED_COLOR, new DyedColorComponent(currentColor));
         }
+    }
+
+    public static ItemStack applyBackAttachmentColor(ItemStack stack, ServerWorld world, Entity entity, BackAttachmentDataComponent backAttachmentDataComponent) {
+        int targetColor;
+        int currentColor = backAttachmentDataComponent.backAttachmentColor();
+        RegistryEntry<Biome> biomeEntry = world.getBiome(entity.getBlockPos());
+
+        if(biomeEntry.getKey().isPresent()) {
+            int color = biomeEntry.value().getFoliageColor();
+            targetColor = color;
+
+            if(currentColor != targetColor) {
+                Color colorA = new Color(currentColor);
+                Color colorB = new Color(targetColor);
+                Color lerpColor = new Color(MathHelper.lerp(COLOR_LERP, colorA.getRed(), colorB.getRed()),
+                        MathHelper.lerp(COLOR_LERP, colorA.getGreen(), colorB.getGreen()),
+                        MathHelper.lerp(COLOR_LERP, colorA.getBlue(), colorB.getBlue()));
+                currentColor = lerpColor.getRGB();
+
+                stack.set(DataComponentTypesME.BACK_ATTACHMENT_DATA, new BackAttachmentDataComponent(backAttachmentDataComponent.backAttachment(), currentColor));
+            }
+        }
+        return stack;
     }
 }
