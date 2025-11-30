de302db84f73fa277a0b2d32a40777a11821a440
Jukoz <olegptk12@gmail.com>
2025-11-30 01:10:23 -0500

    Add smooth transition for mantle

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/MantleOfYavannaItem.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/items/armor/MantleOfYavannaItem.java
@@ -4,27 +4,21 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.DyedColorComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.equipment.EquipmentType;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleTypes;
-import net.minecraft.particle.TintedParticleEffect;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
-import net.sevenstars.middleearth.MiddleEarth;
-import net.sevenstars.middleearth.item.DataComponentTypesME;
-import net.sevenstars.middleearth.item.dataComponents.SeasonDataComponent;
 import net.sevenstars.middleearth.item.utils.armor.ExtendedArmorMaterial;
-import net.sevenstars.middleearth.world.biomes.BiomeTagsME;
 import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
+
 public class MantleOfYavannaItem extends BackAttachmentItem {
-    private int cooldown = 0;
+    private static final float COLOR_LERP = 0.07f;
+    private int currentColor = 0x375729;
+    private int targetColor = 0x375729;
 
     public MantleOfYavannaItem( Settings settings, ExtendedArmorMaterial material) {
         super(settings.armor(material.material(), EquipmentType.CHESTPLATE).maxCount(1), material);
@@ -34,11 +28,22 @@ public class MantleOfYavannaItem extends BackAttachmentItem {
     public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
         super.inventoryTick(stack, world, entity, slot);
         RegistryEntry<Biome> biomeEntry = world.getBiome(entity.getBlockPos());
-        cooldown = Math.max(0, cooldown - 1);
+
         if(biomeEntry.getKey().isPresent()) {
             int color = biomeEntry.value().getFoliageColor();
-            stack.set(DataComponentTypes.DYED_COLOR, new DyedColorComponent(color));
-            cooldown = 10;
+            if(targetColor != color) {
+                targetColor = color;
+            }
+        }
+
+        if(currentColor != targetColor) {
+            Color colorA = new Color(currentColor);
+            Color colorB = new Color(targetColor);
+            Color lerpColor = new Color(MathHelper.lerp(COLOR_LERP, colorA.getRed(), colorB.getRed()),
+                    MathHelper.lerp(COLOR_LERP, colorA.getGreen(), colorB.getGreen()),
+                    MathHelper.lerp(COLOR_LERP, colorA.getBlue(), colorB.getBlue()));
+            currentColor = lerpColor.getRGB();
+            stack.set(DataComponentTypes.DYED_COLOR, new DyedColorComponent(currentColor));
         }
     }
 }
