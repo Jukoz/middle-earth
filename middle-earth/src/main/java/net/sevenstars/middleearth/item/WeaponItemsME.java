21e0bb127ee3300276cd78fc871de045408849b4
Jukoz <olegptk12@gmail.com>
2026-02-07 01:23:30 -0500

    Add WLR buckler shield

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/WeaponItemsME.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/WeaponItemsME.java
@@ -480,6 +480,9 @@ public class WeaponItemsME {
             (settings) -> new CustomLongbowWeaponItem(RangedWeaponTypesME.NOBLE_LONGBOW, settings), new Item.Settings()
                     .component(DataComponentTypesME.FACTION_DATA, new FactionDataComponent(FactionsME.LOTHLORIEN.getValue())));
 
+    public static final Item WOODLAND_REALM_BUCKLER_SHIELD = registerShield("woodland_realm_buckler_shield",
+            (settings) -> new CustomShieldItem(ShieldTypesME.LIGHT_SHIELD, settings), new Item.Settings()
+                    .component(DataComponentTypesME.FACTION_DATA, new FactionDataComponent(FactionsME.LOTHLORIEN.getValue())));
     public static final Item WOODLAND_REALM_SCOUT_SHIELD = registerShield("woodland_realm_scout_shield",
             (settings) -> new CustomShieldItem(ShieldTypesME.MEDIUM_SHIELD, settings), new Item.Settings()
                     .component(DataComponentTypesME.FACTION_DATA, new FactionDataComponent(FactionsME.LOTHLORIEN.getValue())));
