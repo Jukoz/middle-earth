8db2e040d7b5d9e4a8a5745ea41f3991d11a454d
Jukoz <olegptk12@gmail.com>
2026-02-06 23:27:34 -0500

    Add WLR commander shield

--- middle-earth/src/main/java/net/sevenstars/middleearth/item/WeaponItemsME.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/item/WeaponItemsME.java
@@ -510,6 +510,9 @@ public class WeaponItemsME {
     public static final Item WOODLAND_REALM_NIGHTSHADE_ORNAMENTED_SHIELD = registerShield("woodland_realm_nightshade_ornamented_shield",
             (settings) -> new CustomShieldItem(ShieldTypesME.HEAVY_SHIELD, settings), new Item.Settings()
                     .component(DataComponentTypesME.FACTION_DATA, new FactionDataComponent(FactionsME.LOTHLORIEN.getValue())));
+    public static final Item WOODLAND_REALM_COMMANDER_SHIELD = registerShield("woodland_realm_commander_shield",
+            (settings) -> new CustomShieldItem(ShieldTypesME.HEAVY_SHIELD, settings), new Item.Settings()
+                    .component(DataComponentTypesME.FACTION_DATA, new FactionDataComponent(FactionsME.LOTHLORIEN.getValue())));
     //endregion
 
     //region EREBOR
