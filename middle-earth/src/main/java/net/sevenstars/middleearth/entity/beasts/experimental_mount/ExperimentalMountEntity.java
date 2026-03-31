2912b49075bdae4374e3d54455030bb05dcda412
TomSchlom <thomas.huber04@gmail.com>
2026-03-31 20:03:26 +0200

    test turn radius feature

--- middle-earth/src/main/java/net/sevenstars/middleearth/entity/beasts/experimental_mount/ExperimentalMountEntity.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/entity/beasts/experimental_mount/ExperimentalMountEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -55,18 +56,45 @@ public class ExperimentalMountEntity extends MobEntity {
     @Override
     protected void tickControlled(PlayerEntity controllingPlayer, Vec3d movementInput) {
         super.tickControlled(controllingPlayer, movementInput);
+        mountRotation(controllingPlayer, 10.0f, 10.0f, 0);
+
+        this.lastYaw = this.bodyYaw = this.headYaw = this.getYaw();
+
+    }
+
+    protected void mountRotation(PlayerEntity controllingPlayer, float cursorRotationSpeed, float inputRotationSpeed, float triggerAngle) {
         Vec2f rotation = this.getControlledRotation(controllingPlayer);
 
-        if(controllingPlayer.forwardSpeed > 0) {
-            if((rotation.y + 5) < this.getYaw()) {
-                this.setRotation(this.getYaw() - 5, rotation.x);
-            }
-            else if((rotation.y - 5) > this.getYaw()) {
-                this.setRotation(this.getYaw() + 5, rotation.x);
-            }
-            this.lastYaw = this.bodyYaw = this.headYaw = this.getYaw();
+        if(cursorRotationSpeed == 0 && inputRotationSpeed == 0) { // Acts like a horse if both speeds are 0
+            this.setRotation(rotation.y, rotation.x);
         }
+        else {
+            float newRotation = rotation.y % 360;   // Reduce variable to range of -360 to +360
+            newRotation = newRotation < 180 ? newRotation : newRotation - 360;  // Change values from 180 to 360 to their negative counterpart
+            newRotation = newRotation > -180 ? newRotation : newRotation + 360; // Change values from -180 to -360 to their positive counterpart
 
+            float yaw = this.getYaw();
+            yaw = yaw < 180 ? yaw : yaw - 360;  // Change values from 180 to 360 to their negative counterpart
+            yaw = yaw > -180 ? yaw : yaw + 360; // Change values from -180 to -360 to their positive counterpart
+
+            // newRotation and yaw are in a range from -180 to 180
+
+            float difference = yaw - newRotation;
+            if(difference > 180) difference -= 360;
+            else if(difference < -180) difference += 360;   // Check for wrap-around of angles (e.g. -175 and 170)
+
+            if(Math.abs(difference) > triggerAngle) { // Check if difference is greater than the cursors max Angle
+                this.setRotation(yaw - cursorRotationSpeed * Math.signum(difference) * (Math.abs(difference) / 180), rotation.x);
+            }
+
+
+            if(controllingPlayer.sidewaysSpeed < 0) {
+                this.setRotation(this.getYaw() + inputRotationSpeed, rotation.x);
+            }
+            else if(controllingPlayer.sidewaysSpeed > 0) {
+                this.setRotation(this.getYaw() - inputRotationSpeed, rotation.x);
+            }
+        }
     }
 
     @Override
