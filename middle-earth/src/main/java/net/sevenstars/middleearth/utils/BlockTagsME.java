a47b6636968c38ae4a78dc1db154df55a4f48bf0
Yelfra <fran.jelavic@gmail.com>
2026-04-04 12:18:52 +0200

    Refactor BlockTagsME to closer resemble vanilla BlockTags

--- middle-earth/src/main/java/net/sevenstars/middleearth/utils/BlockTagsME.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/utils/BlockTagsME.java
@@ -1,10 +1,15 @@
 package net.sevenstars.middleearth.utils;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.sevenstars.middleearth.MiddleEarth;
 
 public class BlockTagsME {
-    public static TagKey<Block> CURTAINS = TagKey.of(RegistryKeys.BLOCK, IdentifierUtil.create("curtains"));
+    public static final TagKey<Block> CURTAINS = of("curtains");
+
+    private static TagKey<Block> of(String id) {
+        return TagKey.of(RegistryKeys.BLOCK, Identifier.of(MiddleEarth.MOD_ID, id));
+    }
 }
