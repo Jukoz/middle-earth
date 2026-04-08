c4fa0acb89384c3fd3b8e9db58607b673c229782
Jukoz <olegptk12@gmail.com>
2026-04-08 10:54:06 -0400

    Add more papers

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/PaperSheetBlock.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/PaperSheetBlock.java
@@ -3,10 +3,12 @@ package net.sevenstars.middleearth.block.special;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
@@ -15,10 +17,13 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.tick.ScheduledTickView;
@@ -29,6 +34,7 @@ public class PaperSheetBlock extends Block {
     private static final int MAX_PAPERS = 6;
     public static final IntProperty PAPERS = IntProperty.of("papers", 1, MAX_PAPERS);
     public static final EnumProperty<Direction> HORIZONTAL_FACING = Properties.HORIZONTAL_FACING;
+    private static final VoxelShape[] SHAPES_BY_PAPERS;
 
     public PaperSheetBlock(Settings settings) {
         super(settings);
@@ -38,6 +44,11 @@ public class PaperSheetBlock extends Block {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(HORIZONTAL_FACING, PAPERS);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPES_BY_PAPERS[state.get(PAPERS) - 1];
     }
 
     @Override
@@ -52,8 +63,23 @@ public class PaperSheetBlock extends Block {
         return ActionResult.SUCCESS;
     }
 
+    @Override
+    protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if(itemStack.getItem() == this.asItem() && state.get(PAPERS) < MAX_PAPERS) {
+            stack.decrementUnlessCreative(1, player);
+            world.setBlockState(pos, state.cycle(PAPERS));
+            return ActionResult.CONSUME;
+        }
+        return super.onUseWithItem(stack, state, world, pos, player, hand, hit);
+    }
+
+    @Override
     protected boolean canReplace(BlockState state, ItemPlacementContext context) {
-        return !context.shouldCancelInteraction() && context.getStack().getItem() == this.asItem() && state.get(PAPERS) <= MAX_PAPERS ? true : super.canReplace(state, context);
+        boolean bl1 = !context.shouldCancelInteraction();
+        boolean bl2 = context.getStack().getItem() == this.asItem();
+        boolean bl3 = state.get(PAPERS) <= MAX_PAPERS;
+        return bl1 && bl2 && bl3 || super.canReplace(state, context);
     }
 
     public BlockState getPlacementState(ItemPlacementContext ctx) {
@@ -76,5 +102,16 @@ public class PaperSheetBlock extends Block {
 
     public BlockState mirror(BlockState state, BlockMirror mirror) {
         return state.rotate(mirror.getRotation(state.get(HORIZONTAL_FACING)));
+    }
+
+    static {
+        SHAPES_BY_PAPERS = new VoxelShape[]{
+                Block.createColumnShape(8.0, 0.0, 1.0),
+                Block.createColumnShape(12.0, 0.0, 1.0),
+                Block.createColumnShape(14.0, 0.0, 1.0),
+                Block.createColumnShape(16.0, 0.0, 3.0),
+                Block.createColumnShape(16.0, 0.0, 8.0),
+                Block.createColumnShape(16.0, 0.0, 12.0),
+        };
     }
 }
