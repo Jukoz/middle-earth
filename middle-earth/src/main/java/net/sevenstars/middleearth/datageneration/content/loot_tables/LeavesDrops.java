2a8f2f303c0a549ba28b890e58c1e36b3f4e81c5
Jukoz <olegptk12@gmail.com>
2026-05-05 22:08:33 -0400

    Fix weighted saplings and drooping icicles

--- middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/content/loot_tables/LeavesDrops.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/content/loot_tables/LeavesDrops.java
@@ -12,15 +12,9 @@ public class LeavesDrops {
     public static List<LeavesDrop> blocks = new ArrayList<>() {
         {
             add(new LeavesDrop(ModNatureBlocks.LEBETHRON_LEAVES, ModNatureBlocks.LEBETHRON_SAPLING));
-
-            add(new LeavesDrop(ModNatureBlocks.LEBETHRON_LEAVES, ModNatureBlocks.LEBETHRON_SAPLING));
-
             add(new LeavesDrop(ModNatureBlocks.BERRY_HOLLY_LEAVES, ModNatureBlocks.HOLLY_SAPLING));
-
             add(new LeavesDrop(ModNatureBlocks.DRY_LARCH_LEAVES, ModNatureBlocks.LARCH_SAPLING));
-
             add(new LeavesDrop(ModNatureBlocks.FLOWERING_MALLORN_LEAVES, ModNatureBlocks.MALLORN_SAPLING));
-
             add(new LeavesDrop(ModNatureBlocks.DRY_PINE_LEAVES, ModNatureBlocks.PINE_SAPLING));
         }
     };
