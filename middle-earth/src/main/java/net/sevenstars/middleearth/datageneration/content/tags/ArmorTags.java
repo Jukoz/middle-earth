4481860f7ae9845621e787c2228847b9b915ed7f
Jukoz <olegptk12@gmail.com>
2026-05-08 09:05:18 -0400

    Add Moria armor recipes & cleaning code

--- middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/content/tags/ArmorTags.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/datageneration/content/tags/ArmorTags.java
@@ -1,6 +1,9 @@
 package net.sevenstars.middleearth.datageneration.content.tags;
 
 import net.minecraft.item.Item;
+import net.sevenstars.middleearth.block.special.forge.MetalTypes;
+import net.sevenstars.middleearth.item.EquipmentItemsME;
+import net.sevenstars.middleearth.resources.datas.common.DispositionType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,4 +23,42 @@ public class ArmorTags {
     public static List<Item> heavyArmors = new ArrayList<>();
 
     public static List<Item> incompleteArmors = new ArrayList<>();
+
+    // Recipes
+    public static List<RecipeItem> heavyBoots = new ArrayList<>() {
+        {
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.GONDORIAN_PLATE_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.GONDORIAN_KINGS_GUARD_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.GONDORIAN_FOUNTAIN_GUARD_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.EORLING_MARSHAL_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.HORSE_LORD_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.STEEL, EquipmentItemsME.BARDING_PLATED_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.KHAZAD_STEEL, EquipmentItemsME.EREBOR_PLATE_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.KHAZAD_STEEL, EquipmentItemsME.EREBOR_GATEWARDEN_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.KHAZAD_STEEL, EquipmentItemsME.RAVENHILL_SENTINEL_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.KHAZAD_STEEL, EquipmentItemsME.RAVENHILL_WATCHWARDEN_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.EGLADIL_SENTINEL_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.GALADHRIM_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.GALADHRIM_LORD_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.WOODLAND_REALM_SOLDIER_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.WARDEN_OF_THE_GLADE_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.ERYN_GALEN_WATCHWARDEN_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.OXIDISED_ERYN_GALEN_WATCHWARDEN_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.WOODLAND_REALM_COMMANDER_BOOTS));
+            add(new RecipeItem(DispositionType.GOOD, MetalTypes.EDHEL_STEEL, EquipmentItemsME.SILVAN_LORD_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.BLACK_URUK_PLATE_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.BLACK_CASTELLAN_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.DOL_GULDUR_MARAUDER_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.WEATHERED_DOL_GULDUR_MARAUDER_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.URUK_HAI_PLATE_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.STEEL, EquipmentItemsME.ORTHANC_GUARD_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.GUNDABAD_HOBGOBLIN_PLATED_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.MORIA_GOBLIN_SCREECHER_BOOTS));
+            add(new RecipeItem(DispositionType.EVIL, MetalTypes.BURZUM_STEEL, EquipmentItemsME.MORIA_GOBLIN_CHIEF_BOOTS));
+        }
+    };
+
+    public record RecipeItem(DispositionType disposition, MetalTypes metalType, Item output) {
+
+    }
 }
