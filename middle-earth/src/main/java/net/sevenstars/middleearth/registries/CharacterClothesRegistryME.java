33570f89707cb609651876fc8574bc5ff0083f2c
Jukoz <olegptk12@gmail.com>
2026-06-21 18:23:58 -0400

    Update hobbits visuals & new clothings

--- middle-earth/src/main/java/net/sevenstars/middleearth/registries/CharacterClothesRegistryME.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/registries/CharacterClothesRegistryME.java
@@ -10,6 +10,8 @@ public class CharacterClothesRegistryME {
         public final static Identifier PANTS_BROWN                                      = MiddleEarth.of("pants_brown");
         public final static Identifier PANTS_DARK_BROWN                                 = MiddleEarth.of("pants_dark_brown");
         public final static Identifier PANTS_NIGHTSHADE                                 = MiddleEarth.of("pants_nightshade");
+        public final static Identifier SHORT_PANTS_TAN                                  = MiddleEarth.of("short_pants_tan");
+        public final static Identifier SHORT_PANTS_BROWN                                  = MiddleEarth.of("short_pants_brown");
 
         public final static Identifier THONG_BEIGE                                      = MiddleEarth.of("thong_beige");
         public final static Identifier THONG_BROWN                                      = MiddleEarth.of("thong_brown");
@@ -20,15 +22,23 @@ public class CharacterClothesRegistryME {
         public final static Identifier SHIRT_BEIGE                                      = MiddleEarth.of("shirt_beige");
         public final static Identifier SHIRT_NIGHTSHADE                                 = MiddleEarth.of("shirt_nightshade");
         public final static Identifier SHIRT_BURGUNDY                                   = MiddleEarth.of("shirt_burgundy");
+        public final static Identifier SHORT_CHEMISE_WHITE                              = MiddleEarth.of("short_chemise_white");
+        public final static Identifier SHORT_CHEMISE_BEIGE                              = MiddleEarth.of("short_chemise_beige");
 
         public final static Identifier DRESS_BURGUNDY_AND_WHITE                         = MiddleEarth.of("dress_burgundy_and_white");
+        public final static Identifier BROWN_TOWNSMAN_SHIRT                             = MiddleEarth.of("brown_townsman_shirt");
     }
 
     public record Extra(){
+        public final static Identifier EMPTY                                            = MiddleEarth.of("empty");
         public final static Identifier SCARF_BROWN                                      = MiddleEarth.of("scarf_brown");
         public final static Identifier SCARF_DARK_BROWN                                 = MiddleEarth.of("scarf_dark_brown");
         public final static Identifier SCARF_NIGHTSHADE                                 = MiddleEarth.of("scarf_nightshade");
         public final static Identifier SCARF_RED                                        = MiddleEarth.of("scarf_red");
         public final static Identifier SCARF_TEAL                                       = MiddleEarth.of("scarf_teal");
+        public final static Identifier OVERALLS_BROWN                                   = MiddleEarth.of("overalls_brown");
+        public final static Identifier OVERALLS_TAN                                     = MiddleEarth.of("overalls_tan");
+        public final static Identifier BROWN_TOWNSMAN_SHIRT_SHORT                       = MiddleEarth.of("brown_townsman_shirt_short");
+        public final static Identifier GREEN_TOWNSMAN_SHIRT_SHORT                       = MiddleEarth.of("green_townsman_shirt_short");
     }
 }
