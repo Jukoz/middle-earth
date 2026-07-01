cd88db509b0df9f7a2d6faad1d8395d2366858b7
ObliviousCrab <58262600+krahab@users.noreply.github.com>
2026-07-01 15:29:30 -0400

    Add immune entities to damageable plants (players & mobs)

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/ThornyGrowthBlock.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/ThornyGrowthBlock.java
@@ -9,6 +9,8 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.sevenstars.middleearth.MiddleEarthClient;
+import net.sevenstars.middleearth.utils.DamageablePlantsUtil;
 
 public class ThornyGrowthBlock extends GlowLichenBlock {
     public ThornyGrowthBlock(Settings settings) {
@@ -17,13 +19,13 @@ public class ThornyGrowthBlock extends GlowLichenBlock {
 
     @Override
     protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler) {
-        if (entity instanceof LivingEntity && world instanceof ServerWorld serverWorld) {
-            Vec3d movement = entity.isControlledByPlayer() ? entity.getMovement() : entity.getLastRenderPos().subtract(entity.getPos());
+        if (entity instanceof LivingEntity livingEntity && world instanceof ServerWorld serverWorld) {
+            Vec3d movement = livingEntity.isControlledByPlayer() ? livingEntity.getMovement() : livingEntity.getLastRenderPos().subtract(livingEntity.getPos());
             if (movement.horizontalLengthSquared() > 0.0) {
                 double d = Math.abs(movement.getX());
                 double e = Math.abs(movement.getZ());
                 if (d >= 0.003000000026077032 || e >= 0.003000000026077032) {
-                    entity.damage(serverWorld, world.getDamageSources().sweetBerryBush(), 1.0F);
+                    DamageablePlantsUtil.tryDamageEntity(livingEntity, serverWorld, serverWorld.getDamageSources().sweetBerryBush());
                 }
             }
         }
