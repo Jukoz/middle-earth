567e9ae07106f31ffbb7e9186c10d9fad453c745
Jukoz <olegptk12@gmail.com>
2026-08-15 14:34:26 -0400

    Add active & cooldown sarcophagus states

--- middle-earth/src/main/java/net/sevenstars/middleearth/block/special/trial/BarrowTrialSpawner.java
+++ middle-earth/src/main/java/net/sevenstars/middleearth/block/special/trial/BarrowTrialSpawner.java
@@ -1,11 +1,15 @@
 package net.sevenstars.middleearth.block.special.trial;
 
 import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.enums.BedPart;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
@@ -13,11 +17,15 @@ import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.block.WireOrientation;
+import net.minecraft.world.tick.ScheduledTickView;
 import org.jetbrains.annotations.Nullable;
 
 public class BarrowTrialSpawner extends TrialSpawnerBlock {
@@ -85,6 +93,35 @@ public class BarrowTrialSpawner extends TrialSpawnerBlock {
             return VoxelShapes.union(Block.createCuboidShape(0.0, 12.0, 3.0, 16.0, 14.0, 13.0),
                 Block.createColumnShape(16.0, 0, 12.0));
         }
+    }
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        if(!world.isClient()) {
+            BedPart bedPart = state.get(PART);
+            if (bedPart == BedPart.HEAD) {
+                BlockPos neighborBlockPos = pos.offset(getDirectionTowardsOtherPart(bedPart, state.get(FACING)));
+                BlockState neighborBlockState = world.getBlockState(neighborBlockPos);
+                if (neighborBlockState.isOf(this) && neighborBlockState.get(PART) == BedPart.FOOT) {
+                    return state.with(TRIAL_SPAWNER_STATE, neighborState.get(TRIAL_SPAWNER_STATE));
+                }
+            }
+        }
+        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        if (state.get(PART) == BedPart.HEAD) return null;
+        return super.createBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        if (state.get(PART) == BedPart.HEAD) return null;
+        return super.getTicker(world, state, type);
     }
 
     @Override
